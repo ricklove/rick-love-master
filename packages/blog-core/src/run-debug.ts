51bb1bd0805b54e063041700565f4fb56899fe4e
Rick Love <rick@toldpro.com>
2020-05-23 13:48:52 -0500

    vscode debug cross projects

--- packages/blog-core/src/run-debug.ts
+++ packages/blog-core/src/run-debug.ts
@@ -1,8 +1,10 @@
+import { delay } from 'utils/src/delay'
+
 export const run = async () => {
 
     console.log('run START')
 
-    // await delay(1000);
+    await delay(1000);
 
     console.log('run END')
 };
