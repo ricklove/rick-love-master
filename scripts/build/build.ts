8f70f60168bd8dfe365784a9ba9dde05e32917d0
Rick Love <rick@toldpro.com>
2020-05-25 16:58:36 -0500

    Simplify Files

--- scripts/build/build.ts
+++ scripts/build/build.ts
@@ -2,18 +2,25 @@
 import { watchFileChanges, getProjectRootDirectoryPath, getPathNormalized } from 'utils/files';
 import { generateTsconfigPaths, isTsconfigPathDirectory } from './generate-tsconfig-paths';
 
-const build = (rootRaw?: string) => {
+const build = async (rootRaw?: string) => {
 
     const root = getPathNormalized(rootRaw ?? getProjectRootDirectoryPath(__dirname));
     const rootCode = getPathNormalized(root, `./code`);
 
+    const startup = async () => {
+        console.log(`Statup: Regenerate Tsconfig Paths`);
+        await generateTsconfigPaths(root);
+    };
+    await startup();
+
+    console.log(`--- Watching ---`);
     watchFileChanges({ pathRoot: rootCode, runOnStart: false }, async (filesChanged) => {
 
         // console.log(`filesChanged`, filesChanged);
 
         // Update tsconfig
         if (filesChanged.some(x => isTsconfigPathDirectory(x))) {
-            console.log(`Regenerating Tsconfig Paths`, filesChanged);
+            console.log(`Regenerate Tsconfig Paths`, filesChanged);
             generateTsconfigPaths(root);
         }
 
