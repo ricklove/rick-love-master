bcceea5cdf2f0e4d4f3bd7ecb9b3d2a4467e1248
Rick Love <rick@toldpro.com>
2020-05-26 17:55:01 -0500

    Disable Runtime tsconfig-paths

--- code/blog-gatsby-lite/gatsby-config.js
+++ code/blog-gatsby-lite/gatsby-config.js
@@ -3,5 +3,5 @@ require(`source-map-support`).install();
 // support .ts, .tsx
 require(`ts-node`).register({ files: true });
 // Support tsconfig paths
-require(`tsconfig-paths`).register();
+// require(`tsconfig-paths`).register();
 module.exports = require('gatsby-lite/gatsby-config.ts');
