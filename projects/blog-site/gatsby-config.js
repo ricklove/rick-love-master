0d9156ec0ad02b3565a633e6f4856dec41d28e23
Rick Love <rick@toldpro.com>
2020-05-26 20:41:49 -0500

    Rename to blog-site

--- projects/blog-site/gatsby-config.js
+++ projects/blog-site/gatsby-config.js
@@ -2,6 +2,4 @@
 require(`source-map-support`).install();
 // support .ts, .tsx
 require(`ts-node`).register({ files: true });
-// Support tsconfig paths
-// require(`tsconfig-paths`).register();
 module.exports = require('gatsby-lite/gatsby-config.ts');
