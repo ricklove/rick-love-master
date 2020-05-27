f315732bd5a9f6738d16e3fa9ed9e5478d82015c
Rick Love <rick@toldpro.com>
2020-05-26 19:51:08 -0500

    FIX Workspaces

--- scripts/build/process-imports.ts
+++ scripts/build/process-imports.ts
@@ -232,7 +232,7 @@ export const saveDependenciesToModulePackageJson = async (fileDependencies: File
     if (options?.updateRootWorkspaces) {
         const rootPackagePath = getPathNormalized(root, `./package.json`);
         const rootPackageJson = JSON.parse(await readFile(rootPackagePath)) as PackageJson;
-        rootPackageJson.workspaces = packageJsonPaths.map(x => x.replace(`/package.json`, ``));
+        rootPackageJson.workspaces = packageJsonPaths.map(x => x.replace(`/package.json`, ``).replace(`${root}/`, ``));
         await writeFile(rootPackagePath, JSON.stringify(rootPackageJson, null, 2), { overwrite: true, readonly: false });
     }
 };
