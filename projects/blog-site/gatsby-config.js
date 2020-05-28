7956bc6e63e67247ba40ee43bc9629e66e5b18e4
Rick Love <rick@toldpro.com>
2020-05-28 08:33:14 -0500

    Dehydrate All

--- projects/blog-site/gatsby-config.js
+++
@@ -1,5 +0,0 @@
-// support source maps
-require(`source-map-support`).install();
-// support .ts, .tsx
-require(`ts-node`).register({ files: true });
-module.exports = require('gatsby-lite/gatsby-config.ts');
