5a7bc077bd8198d04d4d5096fdb906d204031bfb
Rick Love <rick@toldpro.com>
2020-06-06 12:03:32 -0500

    Add Auth Boilerplate

---
+++ code/auth/client/login.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { theme } from 'themes/theme';
+
+// type PaymentMethodViewServerAccess = {
+//     getPaymentMethods: () => Promise<PaymentMethodClientInfo[]>;
+//     deletePaymentMethod: (key: PaymentMethodStorageKey) => Promise<void>;
+//     onSetupPayment: () => Promise<PaymentProviderSavedPaymentMethodClientSetupToken>;
+//     onPaymentMethodReady: (paymentMethodToken: PaymentProviderSavedPaymentMethodClientToken) => Promise<void>;
+// };
+export const Component = () => {
+    return {
+        LoginView,
+    };
+};
+
+export const LoginView = () => {
+    return (
+        <>
+
+        </>
+    );
+};
