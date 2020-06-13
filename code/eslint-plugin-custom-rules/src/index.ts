a5ad9411d81536c041315ef9d577669edd4965a4
Rick Love <rick@toldpro.com>
2020-06-13 10:28:42 -0500

    Remove Unneeded Rule

--- code/eslint-plugin-custom-rules/src/index.ts
+++ code/eslint-plugin-custom-rules/src/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-default-export */
-import { rule_noUseBeforeDefiveVaruableFunctions } from './no-use-before-define-variable-functions';
+// import { rule_noUseBeforeDefiveVaruableFunctions } from './no-use-before-define-variable-functions.ts.example';
 
 export = {
     rules: {
-        'no-use-before-define-variable-functions': rule_noUseBeforeDefiveVaruableFunctions,
+        // 'no-use-before-define-variable-functions': rule_noUseBeforeDefiveVaruableFunctions,
     },
 };
