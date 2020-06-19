f6358ef7a2a802683b422e13d70489a82bf5ad38
Rick Love <rick@toldpro.com>
2020-06-19 16:00:00 -0500

    FIX Bug

--- code/code-space/code-space-node/run.ts
+++ code/code-space/code-space-node/run.ts
@@ -1,5 +1,7 @@
+import { getProjectRootDirectoryPath, getPathNormalized } from 'utils/files';
 import { loadCodeBase } from '../code-space-logic/load-code-base';
 
 export const run = async () => {
-    await loadCodeBase(`../../../code`);
+    const gitRoot = await getProjectRootDirectoryPath(__dirname);
+    await loadCodeBase(gitRoot, getPathNormalized(gitRoot, `./code`));
 };
