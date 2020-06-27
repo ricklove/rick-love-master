9e78a67cdf1ecbc3796144e964c6d2ca0662d687
Rick Love <rick@toldpro.com>
2020-06-27 16:37:08 -0500

    Create Midi Test

--- code/audio/web-midi-input.ts
+++ code/audio/web-midi-input.ts
@@ -1,13 +1,43 @@
-import { distinct } from 'utils/arrays';
+export class MidiError extends Error {
+    constructor(public message: string, public data?: unknown) { super(); }
+}
+
+export enum MidiCommand {
+    NoteOff = 128,
+    NoteOn = 144,
+};
 
 export type MidiInput = {
-    frequency: number;
+    command: MidiCommand;
+
+    /** Note value from 0-127
+     * 
+     * 88 Key Piano: 21=FirstKey, 108=LastKey, 60=MiddleC
+     */
+    note: number;
+
+    /** Stregth: 1=Softest, 127=Hardest, 0=NoteOff */
+    velocity: number;
+
+    event: MIDIMessageEvent;
 };
-export const createMidiInput = (options: {
-    onKeyDown: (input: MidiInput) => void;
-    onKeyUp: (input: MidiInput) => void;
+export const createMidiInput = async ({
+    onMidiMessage,
+}: {
+    onMidiMessage: (input: MidiInput) => void;
 }) => {
 
-    const test = distinct([1, 2]);
-    return test;
+    if (!navigator.requestMIDIAccess) {
+        throw new MidiError(`WebMIDI is not supported`);
+    }
+
+    const midiAccess = await navigator.requestMIDIAccess();
+    const inputs = midiAccess.inputs.values();
+
+    for (const input of inputs) {
+        input.onmidimessage = (e => {
+            const [command, note, velocity] = e.data;
+            onMidiMessage({ command, note, velocity, event: e });
+        });
+    }
 };
--- code/blog/pageTemplates/component-tests-list.ts
+++ code/blog/pageTemplates/component-tests-list.ts
@@ -3,5 +3,6 @@ export const componentTestList = [
     { name: `payment`, load: async () => (await import(`payments/test/full-stack-test-client`)).PaymentFullStackTesterHost },
     { name: `code-space`, load: async () => (await import(`code-space-views/code-space-component`)).CodeSpaceComponent },
     { name: `auth`, load: async () => (await import(`auth/test/auth-client`)).AuthComponent },
+    { name: `midi-test`, load: async () => (await import(`audio/midi-test`)).MidiTestComponent },
     // { name: `theme`, load: async () => (await import(`themes/theme-example`)).ThemeExample },
 ];
