fcc30d957feb22ea233c130cee1fc0170ee981a2
Rick Love <rick@toldpro.com>
2020-08-27 05:38:56 -0500

    Create Multiples Counting Using Number Words

--- code/blog/pageTemplates/component-games-list.ts
+++ code/blog/pageTemplates/component-games-list.ts
@@ -1,5 +1,6 @@
 export const componentGamesList = [
     { name: `multiples-counting`, load: async () => (await import(`educational-games/multiples-counting`)).EducationalGame_MultiplesCounting },
+    { name: `multiples-counting-words`, load: async () => (await import(`educational-games/multiples-counting-words`)).EducationalGame_MultiplesCountingWords },
     { name: `multiples-monster`, load: async () => (await import(`educational-games/multiples-monster`)).EducationalGame_MultiplesMonster },
     { name: `multiples-snake`, load: async () => (await import(`educational-games/multiples-snake`)).EducationalGame_MultiplesSnake },
 ];
