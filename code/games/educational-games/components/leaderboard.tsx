3d25ddaeefec7ca5247de5602f06618066912e36
Rick Love <rick@toldpro.com>
2020-08-28 17:48:27 -0500

    Extract Leaderboard

---
+++ code/games/educational-games/components/leaderboard.tsx
@@ -0,0 +1,203 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native-lite';
+
+
+export const createLeaderboard = <TScoreState extends {}>(args: {
+    storageKey: string;
+    sortKey: (item: TScoreState) => string | number;
+    scoreColumns: { name: string, getValue: (item: TScoreState) => string }[];
+}) => {
+
+    type LeaderboardScore = { name: string, score: TScoreState };
+    const leaderboard = {
+        saveScore: (name: string, score: TScoreState) => {
+            const data = leaderboard.loadScore();
+
+            data.push({ name, score });
+            data.sort((a, b) => args.sortKey(a.score) < args.sortKey(b.score) ? -1 : 1);
+
+            localStorage.setItem(args.storageKey, JSON.stringify(data));
+        },
+        loadScore: () => {
+            const json = localStorage.getItem(args.storageKey);
+            if (!json) { return []; }
+
+            return JSON.parse(json) as LeaderboardScore[];
+
+            // const scores: LeaderboardScore[] = [
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            //     { name: `Rick`, score: { mistakes: 0, timeMs: 10.78 * 1000 } },
+            // ];
+            // return scores;
+        },
+    };
+
+    const leaderboardInputStyles = {
+        container: {
+            flex: 1,
+            alignItems: `center`,
+            margin: 16,
+        },
+        textInput: {
+            fontFamily: `"Lucida Console", Monaco, monospace`,
+            fontSize: 16,
+            color: `#0000FF`,
+        },
+        buttonView: {
+            margin: 4,
+            padding: 4,
+            borderColor: `#FFFF00`,
+            borderStyle: `solid`,
+            borderWidth: 1,
+            borderRadius: 4,
+        },
+        buttonText: {
+            fontFamily: `"Lucida Console", Monaco, monospace`,
+            fontSize: 16,
+            color: `#FFFF00`,
+        },
+    } as const;
+
+    const LeaderboardNameInput = (props: { onSubmit: (value: string) => void }) => {
+        const [name, setName] = useState(``);
+        return (
+            <View style={leaderboardInputStyles.container}>
+                <TextInput style={leaderboardInputStyles.textInput} value={name} onChange={setName} placeholder='Name' keyboardType='default' autoCompleteType='off' />
+                <TouchableOpacity onPress={() => props.onSubmit(name)}>
+                    <View style={leaderboardInputStyles.buttonView}>
+                        <Text style={leaderboardInputStyles.buttonText}>Save Score</Text>
+                    </View>
+                </TouchableOpacity>
+            </View >
+        );
+    };
+
+
+    const leaderboardStyles = {
+        container: {
+            marginTop: 16,
+            borderColor: `#FFFFFF`,
+            borderStyle: `solid`,
+            borderWidth: 1,
+            borderRadius: 0,
+        },
+        scoreView: {
+            flexDirection: `row`,
+            justifyContent: `space-around`,
+            padding: 4,
+            borderColor: `#FFFFFF`,
+            borderStyle: `solid`,
+            borderWidth: 1,
+            borderRadius: 0,
+        },
+        nameText: {
+            flex: 2,
+            fontFamily: `"Lucida Console", Monaco, monospace`,
+            fontSize: 12,
+            color: `#FFFFFF`,
+            overflow: `hidden`,
+        },
+        scoreText: {
+            flex: 1,
+            textAlign: `right`,
+            fontFamily: `"Lucida Console", Monaco, monospace`,
+            fontSize: 12,
+            color: `#FFFF00`,
+        },
+    } as const;
+
+    const LeaderboardViewInner = (props: { scores: LeaderboardScore[] }) => {
+
+        return (
+            <View style={leaderboardStyles.container}>
+                <View style={leaderboardStyles.scoreView}>
+                    <Text style={leaderboardStyles.nameText}>Leaderboard</Text>
+                    {args.scoreColumns.map(x => (
+                        <Text key={x.name} style={leaderboardStyles.scoreText} />
+                    ))}
+                </View>
+                <View style={leaderboardStyles.scoreView}>
+                    <Text style={leaderboardStyles.nameText}>Name</Text>
+                    {args.scoreColumns.map(x => (
+                        <Text key={x.name} style={leaderboardStyles.scoreText} >{x.name}</Text>
+                    ))}
+                </View>
+                {props.scores.map(x => (
+                    <View style={leaderboardStyles.scoreView}>
+                        <Text style={leaderboardStyles.nameText}>{x.name}</Text>
+                        {args.scoreColumns.map(c => (
+                            <Text key={x.name} style={leaderboardStyles.scoreText} >{c.getValue(x.score)}</Text>
+                        ))}
+                    </View>
+                ))}
+            </View>
+        );
+    };
+
+    // return {
+    //     loadScore: leaderboard.loadScore,
+    //     saveScore: leaderboard.saveScore,
+    //     LeaderboardView,
+    // };
+
+
+    const useLeaderboard = ({
+        getScore,
+    }: {
+        getScore: () => TScoreState;
+    }) => {
+
+        const [leaderboardScores, setLeaderboardScores] = useState({ scores: leaderboard.loadScore() } as { scores: LeaderboardScore[] });
+
+        // const LeaderboardView = (props: {}) => {
+        //     return (
+        //         <>
+        //             <LeaderboardViewInner scores={leaderboardScores.scores} />
+        //         </>
+        //     );
+        // };
+
+
+        // const LeaderboardInputView = (props: { score: TScoreState }) => {
+
+        //     return (
+        //         <>
+        //             {!!gameScore.gameWonTime && (<LeaderboardNameInput onSubmit={onSaveScore} />)}
+        //         </>
+        //     );
+        // };
+
+
+        const LeaderboardArea = (props: { gameOver: boolean, onScoreSaved: () => void }) => {
+
+            const onSaveScore = (name: string) => {
+                leaderboard.saveScore(name, getScore());
+                setLeaderboardScores({ scores: leaderboard.loadScore() });
+                props.onScoreSaved();
+            };
+
+            return (
+                <>
+                    {!!props.gameOver && (<LeaderboardNameInput onSubmit={onSaveScore} />)}
+                    <LeaderboardViewInner scores={leaderboardScores.scores} />
+                </>
+            );
+        };
+
+        return {
+            // LeaderboardView,
+            // LeaderboardNameInput,
+            LeaderboardArea,
+        };
+    };
+
+
+    return {
+        useLeaderboard,
+    };
+};
