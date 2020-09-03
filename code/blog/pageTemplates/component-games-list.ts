7509bc62d39442706f010eb2137a4c8d4221868e
Rick Love <rick@toldpro.com>
2020-09-03 10:27:06 -0500

    Simplify Educational Games List

--- code/blog/pageTemplates/component-games-list.ts
+++ code/blog/pageTemplates/component-games-list.ts
@@ -1,11 +1,5 @@
+import { educationalGamesList } from 'educational-games/_games-list';
+
 export const componentGamesList = [
-    { name: `multiples-counting`, load: async () => (await import(`educational-games/multiples-counting`)).EducationalGame_MultiplesCounting },
-    { name: `multiples-counting-words`, load: async () => (await import(`educational-games/multiples-counting-words`)).EducationalGame_MultiplesCountingWords },
-    { name: `multiples-monster`, load: async () => (await import(`educational-games/multiples-monster`)).EducationalGame_MultiplesMonster },
-    { name: `multiples-snake`, load: async () => (await import(`educational-games/multiples-snake`)).EducationalGame_MultiplesSnake },
-    { name: `multiples-large-board`, load: async () => (await import(`educational-games/multiples-large-board`)).EducationalGame_MultiplesLargeBoard },
-    { name: `multiples-star-blast`, load: async () => (await import(`educational-games/star-blast`)).EducationalGame_StarBlast_Multiples },
-    { name: `multiples-star-blast-sideways`, load: async () => (await import(`educational-games/star-blast-sideways`)).EducationalGame_StarBlastSideways_Multiples },
-    { name: `spanish-star-blast-sideways`, load: async () => (await import(`educational-games/star-blast-sideways-with-subjects/star-blast-spanish`)).EducationalGame_StarBlastSideways_Spanish },
-    { name: `spelling-star-blast-sideways`, load: async () => (await import(`educational-games/star-blast-sideways-with-subjects/star-blast-spelling`)).EducationalGame_StarBlastSideways_Spelling },
+    ...educationalGamesList,
 ];
---
+++ code/games/educational-games/_games-list.ts
@@ -0,0 +1,11 @@
+export const educationalGamesList = [
+    { name: `multiples-counting`, load: async () => (await import(`./multiples-counting`)).EducationalGame_MultiplesCounting },
+    { name: `multiples-counting-words`, load: async () => (await import(`./multiples-counting-words`)).EducationalGame_MultiplesCountingWords },
+    { name: `multiples-monster`, load: async () => (await import(`./multiples-monster`)).EducationalGame_MultiplesMonster },
+    { name: `multiples-snake`, load: async () => (await import(`./multiples-snake`)).EducationalGame_MultiplesSnake },
+    { name: `multiples-large-board`, load: async () => (await import(`./multiples-large-board`)).EducationalGame_MultiplesLargeBoard },
+    { name: `multiples-star-blast`, load: async () => (await import(`./star-blast`)).EducationalGame_StarBlast_Multiples },
+    { name: `multiples-star-blast-sideways`, load: async () => (await import(`./star-blast-sideways`)).EducationalGame_StarBlastSideways_Multiples },
+    { name: `spanish-star-blast-sideways`, load: async () => (await import(`./star-blast-sideways-with-subjects/star-blast-spanish`)).EducationalGame_StarBlastSideways_Spanish },
+    { name: `spelling-star-blast-sideways`, load: async () => (await import(`./star-blast-sideways-with-subjects/star-blast-spelling`)).EducationalGame_StarBlastSideways_Spelling },
+];
