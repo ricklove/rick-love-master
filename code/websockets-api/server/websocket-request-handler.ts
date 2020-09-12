3d68492abb5369c3beb7f08e8c68b97c84c5fc55
Rick Love <rick@toldpro.com>
2020-09-12 11:16:10 -0500

    Add DynamoDb for Websocket Connection Lists

--- code/websockets-api/server/websocket-request-handler.ts
+++ code/websockets-api/server/websocket-request-handler.ts
@@ -1,11 +1,10 @@
 import AWS from 'aws-sdk';
 import { WebsocketConnectionData } from '../client/types';
 
-const s3 = new AWS.S3({ signatureVersion: `v4` });
 
 const settings = {
-    // // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    // bucket: process.env.BUCKET!,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    table: process.env.TABLE!,
 };
 export type WebsocketEvent = {
     requestContext: {
@@ -25,11 +24,69 @@ export const handleWebsocketEvent = async (event: WebsocketEvent) => {
         endpoint: `https://${event.requestContext.domainName}/${event.requestContext.stage}`,
     });
 
+    const connectionIds = await getConnectionIds_DynamoDb({ websocketKey: data.key, connectionId: event.requestContext.connectionId });
+
     // Echo all messages to every client
-    await webSocketClient
-        .postToConnection({
-            ConnectionId: event.requestContext.connectionId,
-            Data: `${event.body}`,
-        })
-        .promise();
+    await Promise.all(connectionIds.connectionIds.map(async () => {
+        return await webSocketClient
+            .postToConnection({
+                ConnectionId: event.requestContext.connectionId,
+                Data: `${event.body}`,
+            })
+            .promise();
+    }));
+
+};
+
+// DynamoDB
+const db = new AWS.DynamoDB();
+const getConnectionIds_DynamoDb = async ({
+    websocketKey,
+    connectionId,
+}: {
+    websocketKey: string;
+    connectionId: string;
+}): Promise<{ connectionIds: string[] }> => {
+
+    type ItemKeyType = {
+        key: {
+            S: string;
+        };
+    };
+    type ItemType = {
+        key: {
+            S: string;
+        };
+        value: {
+            S: string;
+        };
+    };
+    type ValueJsonType = {
+        connectionIds: string[];
+    };
+
+    const key: ItemKeyType = { key: { S: websocketKey } };
+
+    const result = await db.getItem({
+        TableName: settings.table,
+        Key: key,
+    }).promise();
+
+    const existingItem = result.Item as undefined | ItemType;
+    const existingConnectionIds = existingItem ? (JSON.parse(existingItem.value.S) as ValueJsonType).connectionIds : [];
+    let allIds = existingConnectionIds;
+
+    if (!existingConnectionIds.includes(connectionId)) {
+        allIds = [...existingConnectionIds, connectionId];
+        const valueJsonObj: ValueJsonType = {
+            connectionIds: allIds,
+        };
+        const item: ItemType = { ...key, value: { S: JSON.stringify(valueJsonObj) } };
+        await db.putItem({
+            TableName: settings.table,
+            Item: item,
+        }).promise();
+    }
+
+    return { connectionIds: allIds };
 };
