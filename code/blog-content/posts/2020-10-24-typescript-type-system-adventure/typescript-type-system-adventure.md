3af0f5ce29ccda4d45b7fe60b802406e4691c697
Rick Love <rick@toldpro.com>
2020-10-24 22:53:08 -0500

    Adding Content for Typescript Text Adventure

---
+++ code/blog-content/posts/2020-10-24-typescript-type-system-adventure/typescript-type-system-adventure.md
@@ -0,0 +1,14 @@
+---
+title: "Typescript Type System Adventure"
+date: "2020-10-24"
+path: "/typescript-type-system-adventure"
+author: "Rick Love"
+excerpt: "Text Adventure implemented in the Typescript Type System and Vscode JsDoc Viewer"
+---
+
+tl;dr: Play a text adventure by writing code.
+
+### Images
+
+![](seemed-like-a-good-idea.gif)
+![](broken-window-eye.gif)
--- code/typescript-type-system-adventure/game.ts
+++ code/typescript-type-system-adventure/game.ts
@@ -67,6 +67,15 @@ type InventoryContainer<TInventory> = {
 type AddInventoryItem<TGS, TNewInventoryItem> = TGS extends InventoryContainer<infer TOldInventory> ? Omit<TGS, 'inventory'> & InventoryContainer<TOldInventory & TNewInventoryItem> : never;
 type RemoveInventoryItem<TGS, TInventoryItemToRemove extends string> = TGS extends InventoryContainer<infer TOldInventory> ? Omit<TGS, 'inventory'> & InventoryContainer<Omit<TOldInventory, TInventoryItemToRemove>> : never;
 
+type YouAreDead<TReason extends string> = {
+    /** ### You Are Dead
+     * 
+     * Do not pass go, do not collect $200
+     * 
+     * You did not win.
+     */
+    youAreDead: TReason;
+};
 // type r01 = { inventory: { letter: true } };
 // const c01: r01 = null as unknown as r01;
 // type r02 = RemoveInventoryItem<r01, 'letter'>;
@@ -180,6 +189,22 @@ type OutsideOldHouse = {
      * 
      */
     look: <T>(this: T) => T;
+
+    /** ### You break the window
+     * 
+     * Maybe if you break the window, you will be able to crawl through it to get into the house.
+     * 
+     * However, you quickly regret your decision.
+     * 
+     * ![](https://ricklove.me/blog-content/posts/2020-10-24-typescript-type-system-adventure/broken-window-eye.gif)
+     * 
+     */
+    breakWindow: <T>(this: T) => YouAreDead<'I guess people really do defend their property'>;
+
+    knockOnDoor: <T>(this: T) => T;
+    ringDoorBell: <T>(this: T) => T;
+
+
 };
 
 // Play Game (Test)
@@ -193,6 +218,7 @@ const play = (): GameWon => {
         .look()
         .approachHouse()
         .look()
+        .breakWindow()
         ;
 
     return result;
