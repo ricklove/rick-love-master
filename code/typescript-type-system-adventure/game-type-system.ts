8ee926b7255e88275dbcc01268d447b73b4dcd10
Rick Love <rick@toldpro.com>
2020-10-26 14:02:30 -0500

    Finish Type System Game

--- code/typescript-type-system-adventure/game-type-system.ts
+++ code/typescript-type-system-adventure/game-type-system.ts
@@ -62,7 +62,7 @@ type Hider = `Typescript Type System Adventure - by Rick Love 🤓​​​​�
 export type GameStart = Hider & GameStateCommon & { hasGameStarted: false };
 
 type GameStateCommon<TLocation extends string = string, TInventory extends {} = {}, TEnvironment extends {} = {}> = {
-    // command: <T extends GameStateCommon, TCommand>(this: T, command: TCommand) => Command<T, TCommand>;
+    command: <T extends GameStateCommon, TCommand extends string>(this: T, command: TCommand) => Command<T, TCommand>;
     location: TLocation;
     inventory: TInventory;
     environment: TEnvironment;
@@ -70,15 +70,6 @@ type GameStateCommon<TLocation extends string = string, TInventory extends {} = 
 type SetLocation<T extends GameStateCommon, TNew extends string> = { location: TNew } & Omit<T, 'location'>;
 type SetInventory<T extends GameStateCommon, TNew> = { inventory: TNew & Omit<T['inventory'], keyof TNew> } & Omit<T, 'inventory'>;
 type SetEnvironment<T extends GameStateCommon, TNew> = { environment: TNew & Omit<T['environment'], keyof TNew> } & Omit<T, 'environment'>;
-
-type UnknownCommand<T> = {
-    /** ### Unknown Command
-     * 
-     * I don't know what you mean. Try Again.
-     * 
-     */
-    execute: T;
-};
 
 type Location_InFrontOfHouse = 'In Front of House';
 type Location_OutsideOldHouse = 'Outside Old House';
@@ -96,6 +87,24 @@ export type Command<TState extends GameStateCommon, TCommand> =
         execute: { inventory: TState['inventory'] } & TState;
     }
     : Hider &
+
+    // Game Start
+    [TCommand, TState] extends ['help', {}] ?
+    {
+        /** ### Help
+         * 
+         * Commands to try:
+         * 
+         * - look
+         * - inventory
+         * - open mailbox
+         * - open envelope
+         * - read letter
+         * - move house
+         * 
+         */
+        execute: TState;
+    } :
 
     // Game Start
     [TCommand, TState] extends ['begin', { hasGameStarted: false }] ?
@@ -140,6 +149,16 @@ export type Command<TState extends GameStateCommon, TCommand> =
          * 'To: The Finder of This Letter'
          */
         execute: Hider & SetInventory<SetEnvironment<TState, { mailbox: 'open' }>, { envelope: true }>;
+    }
+    : [TCommand, TState] extends ['open mailbox', { location: Location_InFrontOfHouse, environment: { mailbox: 'open' } }] ?
+    {
+        /** ### Mailbox Already Open
+         * 
+         * The mailbox is already open.
+         * 
+         * Really, you just opened it a second ago.
+         */
+        execute: TState;
     }
 
     // Envelope
@@ -207,6 +226,16 @@ export type Command<TState extends GameStateCommon, TCommand> =
          */
         execute: TState;
     }
+    : [TCommand, TState] extends ['look house', { location: Location_InFrontOfHouse, inventory: { letter: 'read' } }] ?
+    {
+        /** ### Look at Old House
+         * 
+         * Yeah, that is an old house.
+         * 
+         * Maybe you should ***walk*** up to it.
+         */
+        execute: TState;
+    }
     : [TCommand, TState] extends ['approach house' | 'move house' | 'walk house', { location: Location_InFrontOfHouse, inventory: { letter: 'read' } }] ?
     {
         /** ### Approach House
@@ -221,7 +250,7 @@ export type Command<TState extends GameStateCommon, TCommand> =
     }
 
     // House Door
-    : [TCommand, TState] extends ['look', { location: Location_OutsideOldHouse, inventory: { letter: 'read' } }] ?
+    : [TCommand, TState] extends ['look', { location: Location_OutsideOldHouse }] ?
     {
         /** ### Outside Old House
          * 
@@ -236,17 +265,99 @@ export type Command<TState extends GameStateCommon, TCommand> =
          */
         execute: TState;
     }
-    : [TCommand, TState] extends ['approach house' | 'move house' | 'walk house', { location: Location_InFrontOfHouse, inventory: { letter: 'read' } }] ?
+    : [TCommand, TState] extends ['break window', { location: Location_OutsideOldHouse }] ?
     {
-        /** ### Approach House
+        /** ### Break the Window
          * 
-         * You walk towards the old house.
+         * Maybe if you break the window, you will be able to crawl through it to get into the house.
          * 
-         * You hear the wind rustling through the trees, and a branch scratching against a window in the back of the house.
+         * However, you quickly regret your decision...
          * 
-         * As you get near, the flickering light buzzes and finally goes out.
          */
-        execute: SetLocation<TState, Location_OutsideOldHouse>;
+        execute: {
+            /** ### You Are Dead
+             * 
+             * Breaking that window may have been a little aggressive.
+             * 
+             * At least it seems that whatever was inside the house thought so.
+             * 
+             * You don't know exactly what happened, but you are dead.
+             * 
+             * ![](https://ricklove.me/blog-content/posts/2020-10-24-typescript-type-system-adventure/broken-window-eye.gif)
+             */
+            youAreDead: GameOver;
+        };
+    }
+    : [TCommand, TState] extends ['open door', { location: Location_OutsideOldHouse }] ?
+    {
+        /** ### Open the Door
+         * 
+         * Just make yourself at home! 
+         * 
+         * Or maybe not...
+         */
+        execute: {
+            /** ### You Are Dead
+             * 
+             * The owners didn't appreciate your rude entrance and neither did their shotgun.
+             * 
+             * You are dead.
+             * 
+             */
+            youAreDead: GameOver;
+        };
+    }
+    : [TCommand, TState] extends ['ring doorbell', { location: Location_OutsideOldHouse }] ?
+    {
+        /** ### Ring the Doorbell
+         * 
+         * You press the button for the doorbell, but don't hear anything.
+         * 
+         * After waiting a moment, nothing seems to have happened.
+         * 
+         */
+        execute: TState;
+    }
+    : [TCommand, TState] extends ['knock door', { location: Location_OutsideOldHouse }] ?
+    {
+        /** ### Knock on the Door
+         * 
+         * You knock on the door.
+         * 
+         * It appears someone is home, you hear footsteps inside...
+         */
+        execute: {
+            /** * ### Wait at Door
+             * 
+             * After waiting a moment, a nice old lady appears:
+             * 
+             * > Welcome!
+             * >
+             * > I always like it when children visit.
+             * > 
+             * > I just got done baking some chocolate chip cookies.
+             */
+            wait: {
+                /** ### Eat the Cookie
+                 * 
+                 * You take a cookie from the plate the lady is pushing towards you.
+                 * 
+                 * You aren't sure about it, but decide that it's better not to be rude.
+                 */
+                eatCookie: {
+                    /** ### You are Now Being Tracked
+                     * 
+                     * You accepted the cookies.
+                     * 
+                     * Now everywhere you go you see ads about the last thing that you mentioned to your friend in private conversation.
+                     * 
+                     * Oh well, worse things could have happened.
+                     * 
+                     */
+                    youAreNowBeingTracked: GameWon;
+                };
+            };
+        };
     }
 
     // The Unknown
@@ -260,7 +371,6 @@ export type Command<TState extends GameStateCommon, TCommand> =
          */
         execute: TState;
     }
-
     // The Any
     : [TCommand, TState] extends ['any', {}] ? {
         /** ### `any` is Evil
@@ -275,48 +385,86 @@ export type Command<TState extends GameStateCommon, TCommand> =
          * 
          * https://ricklove.me/typescript-any-is-evil
          * 
-         * ```text
-         * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-         * MMMMMMMMMMMM        MMMMMMMMMMMM
-         * MMMMMMMMMM            MMMMMMMMMM
-         * MMMMMMMMM              MMMMMMMMM
-         * MMMMMMMM                MMMMMMMM
-         * MMMMMMM                 MMMMMMMM
-         * MMMMMMM                  MMMMMMM
-         * MMMMMMM                  MMMMMMM
-         * MMMMMMM    MMM    MMM    MMMMMMM
-         * MMMMMMM   MMMMM   MMMM   MMMMMMM
-         * MMMMMMM   MMMMM   MMMM   MMMMMMM
-         * MMMMMMMM   MMMM M MMMM  MMMMMMMM
-         * MMVKMMMM        M        MMMMMMM
-         * MMMMMMMM       MMM      MMMMMMMM
-         * MMMMMMMMMMMM   MMM  MMMMMMMMMMMM
-         * MMMMMMMMMM MM       M  MMMMMMMMM
-         * MMMMMMMMMM  M M M M M MMMMMMMMMM
-         * MMMM  MMMMM MMMMMMMMM MMMMM   MM
-         * MMM    MMMM M MMMMM M MMMM    MM
-         * MMM    MMMM   M M M  MMMMM   MMM
-         * MMMM    MMMM         MMM      MM
-         * MMM       MMMM     MMMM       MM
-         * MMM         MMMMMMMM      M  MMM
-         * MMMM  MMM      MMM      MMMMMMMM
-         * MMMMMMMMMMM  MM       MMMMMMM  M
-         * MMM  MMMMMMM       MMMMMMMMM   M
-         * MM    MMM        MM            M
-         * MM            MMMM            MM
-         * MMM        MMMMMMMMMMMMM       M
-         * MM      MMMMMMMMMMMMMMMMMMM    M
-         * MMM   MMMMMMMMMMMMMMMMMMMMMM   M
-         * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-         * ```
-         * Ascii Art From: https://asciiart.website/index.php?art=people/skeletons
-         * 
          * */
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         execute: any;
     }
     // Unknown
-    : UnknownCommand<TState>;
+    : 'UNKNOWN COMMAND - Try Again' & {
+        /** ### Unknown Command
+         * 
+         * I don't know what you mean. Try Again.
+         * 
+         */
+        execute: TState;
+    };
+
+type GameOver = {
+    /** ### GAME OVER!
+     * 
+     * You are dead!
+     * 
+     * You regret your decisions. Try again.
+     * 
+     * https://ricklove.me/typescript-type-system-adventure
+     * 
+     * ```text
+     * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+     * MMMMMMMMMMMM        MMMMMMMMMMMM
+     * MMMMMMMMMM            MMMMMMMMMM
+     * MMMMMMMMM              MMMMMMMMM
+     * MMMMMMMM                MMMMMMMM
+     * MMMMMMM                 MMMMMMMM
+     * MMMMMMM                  MMMMMMM
+     * MMMMMMM                  MMMMMMM
+     * MMMMMMM    MMM    MMM    MMMMMMM
+     * MMMMMMM   MMMMM   MMMM   MMMMMMM
+     * MMMMMMM   MMMMM   MMMM   MMMMMMM
+     * MMMMMMMM   MMMM M MMMM  MMMMMMMM
+     * MMVKMMMM        M        MMMMMMM
+     * MMMMMMMM       MMM      MMMMMMMM
+     * MMMMMMMMMMMM   MMM  MMMMMMMMMMMM
+     * MMMMMMMMMM MM       M  MMMMMMMMM
+     * MMMMMMMMMM  M M M M M MMMMMMMMMM
+     * MMMM  MMMMM MMMMMMMMM MMMMM   MM
+     * MMM    MMMM M MMMMM M MMMM    MM
+     * MMM    MMMM   M M M  MMMMM   MMM
+     * MMMM    MMMM         MMM      MM
+     * MMM       MMMM     MMMM       MM
+     * MMM         MMMMMMMM      M  MMM
+     * MMMM  MMM      MMM      MMMMMMMM
+     * MMMMMMMMMMM  MM       MMMMMMM  M
+     * MMM  MMMMMMM       MMMMMMMMM   M
+     * MM    MMM        MM            M
+     * MM            MMMM            MM
+     * MMM        MMMMMMMMMMMMM       M
+     * MM      MMMMMMMMMMMMMMMMMMM    M
+     * MMM   MMMMMMMMMMMMMMMMMMMMMM   M
+     * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+     * ```
+     * Ascii Art From: https://asciiart.website/index.php?art=people/skeletons
+     * 
+     * */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    gameOver: GameOver;
+}
+
+type GameWon = {
+
+    /** ### Winner!
+     * 
+     * You have won! 
+     * 
+     * Great job!
+     * I hope this was fun.
+     * 
+     * **Please share it with all your nerd friends!**
+     * 
+     * https://ricklove.me/typescript-type-system-adventure
+     * 
+     * */
+    winner: GameWon;
+};
 
 // Play Game
 type Step00 = GameStart;
@@ -334,12 +482,27 @@ type Step07d = Command<Step07, 'any'>['execute'];
 type Step08 = Command<Step07b, 'walk house'>['execute'];
 type Step08b = Command<Step08, 'look'>['execute'];
 
-// const play = () => {
-//     const gameStart = null as unknown as GameStart;
-//     gameStart.command(`begin`).execute
-//         .command(`look`).execute
-//         ;
-// };
+const play = () => {
+    const gameStart = null as unknown as GameStart;
+    const result = gameStart
+        .command(`begin`).execute
+        .command(`look`).execute
+        .command(`open mailbox`).execute
+        .command(`look`).execute
+        .command(`inv`).execute
+        .command(`open envelope`).execute
+        .command(`read letter`).execute
+        .command(`open mailbox`).execute
+        .command(`look`).execute
+        .command(`look house`).execute
+        .command(`walk house`).execute
+        .command(`look`).execute
+        // .command(`break window`).execute.youAreDead.gameOver
+        // .command(`open door`).execute.youAreDead.gameOver
+        .command(`ring doorbell`).execute
+        .command(`knock door`).execute.wait.eatCookie.youAreNowBeingTracked.winner.winner
+        ;
+};
 
 // // Play Game (Test)
 // const play = () => {
