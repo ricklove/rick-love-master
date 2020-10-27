d993e47b07983243563e179c1249472cfa203259
Rick Love <rick@toldpro.com>
2020-10-27 17:11:01 -0500

    FIX User Select

--- code/blog/pageTemplates/layout/layout-game-css.tsx
+++ code/blog/pageTemplates/layout/layout-game-css.tsx
@@ -1,6 +1,11 @@
-import './layout-game.css';
-import React from 'react';
+// import './layout-game.css';
+import React, { useEffect } from 'react';
 
 export const LayoutGameCss = (props: {}) => {
+
+    useEffect(() => {
+        document.body.style.userSelect = `none`;
+        document.body.style.webkitUserSelect = `none`;
+    }, []);
     return (<></>);
 };
