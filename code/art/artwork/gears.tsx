789228eaf22248a70b99c9d61032181d48d797dd
Rick Love <rick@toldpro.com>
2021-03-01 20:35:11 -0600

    Improve Gears

--- code/art/artwork/gears.tsx
+++ code/art/artwork/gears.tsx
@@ -51,7 +51,33 @@ const drawGear = (g: p5,
     const jitterSize = 5;
     const angle0 = rotationAngle;
 
+    for (let j = 0; j < 1; j++) {
+
+        // g.fill(color.h, color.s, color.b, 0.25);
+        g.fill(0, 0, 0);
+        g.curveTightness(0.9);
+        g.beginShape();
+        for (let i = 0; i <= teeth; i++) {
+            // g.curveVertex(x + diaI * g.cos((i + 0) * toothAngle), y + diaI * g.sin((i + 0) * toothAngle));
+            if (i !== 0) {
+                g.curveVertex(x + radI * g.cos((i + 0.2) * toothAngle + angle0) + jitterSize * random(), y + radI * g.sin((i + 0.2) * toothAngle + angle0) + jitterSize * random());
+            }
+
+            g.curveVertex(x + radO * g.cos((i + 0.3) * toothAngle + angle0) + jitterSize * random(), y + radO * g.sin((i + 0.3) * toothAngle + angle0) + jitterSize * random());
+            g.curveVertex(x + radO * g.cos((i + 0.7) * toothAngle + angle0) + jitterSize * random(), y + radO * g.sin((i + 0.7) * toothAngle + angle0) + jitterSize * random());
+            g.curveVertex(x + radI * g.cos((i + 0.8) * toothAngle + angle0) + jitterSize * random(), y + radI * g.sin((i + 0.8) * toothAngle + angle0) + jitterSize * random());
+            // g.curveVertex(x + diaI * g.cos((i + 1) * toothAngle), y + diaI * g.sin((i + 1) * toothAngle));
+            // g.arc(x, y, diaI, diaI, (i + 0) * toothAngle, (i + 0.5) * toothAngle);
+            // g.arc(x, y, diaO, diaO, (i + 0.5) * toothAngle, (i + 1) * toothAngle);
+        }
+        g.endShape();
+    }
+
     for (let j = 0; j < 4; j++) {
+
+        g.noFill();
+        // g.fill(0, 0, 0, 0.1);
+        // g.fill(color.h, color.s, color.b, 0.05);
         g.curveTightness(0.9);
         g.beginShape();
         for (let i = 0; i <= teeth; i++) {
@@ -70,6 +96,7 @@ const drawGear = (g: p5,
         g.endShape();
 
         // Inner circle
+        g.fill(0, 0, 0);
         g.beginShape();
         for (let i = 0; i <= teeth + 2; i++) {
             g.curveVertex(x + radiusAxis * g.cos((i + 0.3) * toothAngle + angle0) + jitterSize * random(), y + radiusAxis * g.sin((i + 0.3) * toothAngle + angle0) + jitterSize * random());
@@ -88,43 +115,45 @@ export const art_gears = {
         return null;
     },
     renderArt: (hostElement: HTMLElement, hash = `This is my hash!`) => {
-        // const { a, b, c } = { a: 57, b: 23, c: 15 };
-
         const { random: randomMain } = createRandomGenerator(hash);
-        // const { a, b, c } = { a: 1 + Math.floor(57 * random()), b: 1 + Math.floor(213 * random()), c: 1 + Math.floor(115 * random()) };
-        // const { cr, cg, cb, ca } = { cr: Math.floor(25 + 230 * random()), cg: Math.floor(25 + 230 * random()), cb: Math.floor(25 + 230 * random()), ca: Math.floor(25 + 25 * random()) };
 
         let tick = 0;
 
         const canvasSize = 300;
+        const halfSize = canvasSize * 0.5;
         const minGearRadius = canvasSize / 16;
-        const maxGearRadius = canvasSize / 2;
+        const maxGearRadius = canvasSize / 3;
+        const minGearCount = 8;
         const maxGearCount = 16;
-        const gearCount = Math.floor(1 + maxGearCount * randomMain());
+        const gearCount = Math.floor(minGearCount + (maxGearCount - minGearCount) * randomMain());
 
         let lastGear = {
-            position: { x: canvasSize * 0.5, y: canvasSize * 0.5 },
+            position: { x: halfSize, y: halfSize },
             radius: minGearRadius,
             // position: { x: canvasSize * randomMain(), y: canvasSize * randomMain() },
             // size: minGearSize + (maxGearSize - minGearSize) * randomMain(),
             randomSeed: `${randomMain()}`,
+            color: { h: 360 * randomMain(), s: 50, b: 50, a: 0.5 },
         };
         const gears = [...new Array(gearCount)].map(() => {
 
             const targetRadius = minGearRadius + (maxGearRadius - minGearRadius) * randomMain();
             const targetDistance = lastGear.radius + targetRadius;
-            const targetAngle = Math.PI / 2 * Math.random() +
-                (lastGear.position.x > canvasSize * 0.5 && lastGear.position.y > canvasSize * 0.5 ? Math.PI * 1
-                    : lastGear.position.x < canvasSize * 0.5 && lastGear.position.y > canvasSize * 0.5 ? Math.PI * 0.5
-                        : lastGear.position.x > canvasSize * 0.5 && lastGear.position.y < canvasSize * 0.5 ? Math.PI * 1.5
+            const lastPos = lastGear.position;
+
+            // Try to bring back to center
+            const targetAngle = Math.PI / 2 * randomMain() +
+                (lastPos.x > halfSize && lastPos.y > halfSize ? Math.PI * 1
+                    : lastPos.x < halfSize && lastPos.y > halfSize ? Math.PI * 0.5
+                        : lastPos.x > halfSize && lastPos.y < halfSize ? Math.PI * 1.5
                             : 0);
 
             const pos = {
-                x: lastGear.position.x + targetDistance * Math.cos(targetAngle),
-                y: lastGear.position.y + targetDistance * Math.sin(targetAngle),
+                x: lastPos.x + targetDistance * Math.cos(targetAngle),
+                y: lastPos.y + targetDistance * Math.sin(targetAngle),
             };
-            const xDelta = pos.x - lastGear.position.x;
-            const yDelta = pos.y - lastGear.position.y;
+            const xDelta = pos.x - lastPos.x;
+            const yDelta = pos.y - lastPos.y;
             const actualRadius = Math.sqrt(xDelta * xDelta + yDelta * yDelta) - lastGear.radius;
 
             // eslint-disable-next-line no-return-assign
@@ -132,6 +161,7 @@ export const art_gears = {
                 position: pos,
                 radius: actualRadius,
                 randomSeed: `${randomMain()}`,
+                color: { h: 360 * randomMain(), s: 50, b: 50, a: 0.5 },
             };
         });
 
@@ -158,7 +188,7 @@ export const art_gears = {
                         radiusOuter: gear.radius + teethDepth,
                         radiusAxis: gear.radius * 0.15,
                         teeth,
-                        color: { h: 15 * i, s: 50, b: 50, a: 0.5 },
+                        color: gear.color,
                         rotationAngle: direction * rotationAngle,
                         randomSeed: gear.randomSeed,
                     });
