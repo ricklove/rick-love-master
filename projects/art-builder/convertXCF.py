c7f620799cb7aa2fbd6e1e3c262c94ebce4ef601
Rick Love <rick@toldpro.com>
2021-08-16 20:27:35 -0500

    Add function

--- projects/art-builder/convertXCF.py
+++ projects/art-builder/convertXCF.py
@@ -6,6 +6,35 @@ import sys
 import time
 from gimpfu import *
 
+i = 0
+
+
+def save_layer(image, l, layersDir):
+    global i
+    print "Saving layer %s" % l.name
+
+    outfile = layersDir + '/' + str(i) + '-' + l.name + '.png'
+
+    print "Saving to %s" % outfile
+    pdb.file_png_save(image, l, outfile, outfile, True,
+                      9, True, True, True, True, True)
+    print "Saved to %s" % outfile
+    i = i + 1
+
+
+# def save_layer_or_group(layer):
+#     # check if layer is a group and drill down if it is
+#     if pdb.gimp_item_is_group(layer):
+#         gr = layer
+#         gr_items = pdb.gimp_item_get_children(layer)
+#         for index in gr_items[1]:
+#             item = gimp.Item.from_id(index)
+
+#     # if layer is on the first level
+#     if layer.name == name:
+#         return layer
+#         return None
+
 
 def process(infile):
 
@@ -18,15 +47,8 @@ def process(infile):
     if not os.path.exists(layersDir):
         os.mkdir(layersDir)
 
-    for i, l in enumerate(image.layers):
-        print "Saving layer %s" % l.name
-
-        outfile = layersDir + '/' + str(i) + '-' + l.name + '.png'
-
-        print "Saving to %s" % outfile
-        pdb.file_png_save(image, l, outfile, outfile, True,
-                          9, True, True, True, True, True)
-        print "Saved to %s" % outfile
+    for l in image.layers:
+        save_layer(image, l, layersDir)
 
     pdb.gimp_image_delete(image)
 
