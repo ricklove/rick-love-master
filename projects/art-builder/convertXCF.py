f2f1640ac13e83a4576622e8f139c92cc4be7c60
Rick Love <rick@toldpro.com>
2021-08-16 20:34:43 -0500

    Extract layers

--- projects/art-builder/convertXCF.py
+++ projects/art-builder/convertXCF.py
@@ -6,34 +6,34 @@ import sys
 import time
 from gimpfu import *
 
-i = 0
+nextIndex = 0
 
 
-def save_layer(image, l, layersDir):
-    global i
-    print "Saving layer %s" % l.name
+def save_layer(image, layer, layersDir):
+    global nextIndex
+    print "Saving layer %s" % layer.name
 
-    outfile = layersDir + '/' + str(i) + '-' + l.name + '.png'
+    outfile = layersDir + '/' + str(nextIndex) + '-' + layer.name + '.png'
 
     print "Saving to %s" % outfile
-    pdb.file_png_save(image, l, outfile, outfile, True,
+    pdb.file_png_save(image, layer, outfile, outfile, True,
                       9, True, True, True, True, True)
     print "Saved to %s" % outfile
-    i = i + 1
+    nextIndex = nextIndex + 1
 
 
-# def save_layer_or_group(layer):
-#     # check if layer is a group and drill down if it is
-#     if pdb.gimp_item_is_group(layer):
-#         gr = layer
-#         gr_items = pdb.gimp_item_get_children(layer)
-#         for index in gr_items[1]:
-#             item = gimp.Item.from_id(index)
+def save_layer_or_group(image, layer, layersDir):
+    # check if layer is a group and drill down if it is
+    if pdb.gimp_item_is_group(layer):
+        gr = layer
+        gr_items = pdb.gimp_item_get_children(layer)
+        for index in gr_items[1]:
+            item = gimp.Item.from_id(index)
+            save_layer(image, item, layersDir)
+            return
 
-#     # if layer is on the first level
-#     if layer.name == name:
-#         return layer
-#         return None
+    # if not a group
+    save_layer(image, layer, layersDir)
 
 
 def process(infile):
@@ -48,7 +48,7 @@ def process(infile):
         os.mkdir(layersDir)
 
     for l in image.layers:
-        save_layer(image, l, layersDir)
+        save_layer_or_group(image, l, layersDir)
 
     pdb.gimp_image_delete(image)
 
