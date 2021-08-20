1c24c6e9b54e0eada8831c1bf2b5794b9e30df29
Rick Love <rick@toldpro.com>
2021-08-20 01:21:53 -0500

    Decent render, inkscape gui is best

--- projects/art-builder/src/build-vector-art.ts
+++ projects/art-builder/src/build-vector-art.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { promisify } from 'util';
 import { injectSvgComponents } from './vector/inject-svg-components';
 import { processImageLayerFiles } from './process-image-layers';
-// import { renderSvg } from './vector/render-svg';
+import { renderSvg } from './vector/render-svg';
 
 const fs = fsRaw.promises;
 
@@ -12,7 +12,7 @@ const fs = fsRaw.promises;
 export const buildVectorArt = async (svgDirectory: string, outDir: string, options?: { skipExtraction?: boolean }) => {
 
     await injectSvgComponents(svgDirectory);
-    // await renderSvg(svgDirectory, outDir);
+    await renderSvg(svgDirectory, outDir);
 
 
     console.log(`DONE`);
--- projects/art-builder/src/vector/render-svg.ts
+++ projects/art-builder/src/vector/render-svg.ts
@@ -1,47 +1,51 @@
 import fsRaw from 'fs';
 import path from 'path';
-import xmljs from 'xml-js';
-import svgexportRaw from 'svgexport';
-import { loadImageFile, saveImageFile } from '../image-files';
+import sharp from 'sharp';
 
 const fs = fsRaw.promises;
 const normalizeFilePath = (filePath: string) => path.resolve(filePath).replace(/\\/g, `/`);
 
-type SvgExportDataFile = {
-    input: [
-        inputfile: string,
-        format: 'png',
-        /** 1-100 % */
-        quality: number,
-        size:`${number}:${number}`
-    ];
-    output: [outputFile:string];
-}[];
-const svgexport = svgexportRaw as {
-    render: (datafile: SvgExportDataFile, callback: () => void) => void;
-};
-
-// export const renderSvg = async (sourceDir: string, destDir: string) => {
-//     console.log(`# Render Svg `, { sourceDir, destDir });
-
-//     const dirFiles = await fs.readdir(sourceDir, { withFileTypes: true });
-//     const svgFilePaths = dirFiles
-//         .filter(x => x.name.endsWith(`.svg`))
-//         .map(x => normalizeFilePath(path.join(sourceDir, x.name)))
-//         .sort((a, b) => a.localeCompare(b));
-
-
-//     await Promise.all(svgFilePaths.map(async (x) => {
-//         await new Promise<void>((resolve, reject) => {
-//             console.log(`renderSvg - render svg`, { x });
-
-//             svgexport.render([{
-//                 input: [x, `png`, 100, `32:32`],
-//                 output: [x + `.png`],
-//             }], () => {
-//                 resolve();
-//             });
-//         });
-//     }));
-
+// type SvgExportDataFile = {
+//     input: [
+//         inputfile: string,
+//         format: 'png',
+//         /** 1-100 % */
+//         quality: number,
+//         size:`${number}:${number}`
+//     ];
+//     output: [outputFile:string];
+// }[];
+// const svgexport = svgexportRaw as {
+//     render: (datafile: SvgExportDataFile, callback: () => void) => void;
 // };
+
+export const renderSvg = async (sourceDir: string, destDir: string) => {
+    console.log(`# Render Svg `, { sourceDir, destDir });
+
+    const dirFiles = await fs.readdir(sourceDir, { withFileTypes: true });
+    const svgFilePaths = dirFiles
+        .filter(x => x.name.endsWith(`.svg`))
+        .map(x => normalizeFilePath(path.join(sourceDir, x.name)))
+        .sort((a, b) => a.localeCompare(b));
+
+
+    await Promise.all(svgFilePaths.map(async (x) => {
+        console.log(`renderSvg - render svg`, { x });
+        await sharp(x, { density: 96 * 4 })
+            .resize(32, 32, { kernel: `nearest`, withoutEnlargement: true })
+            .png({ dither: 0 })
+            .toFile(`${x}.png`);
+
+        // await new Promise<void>((resolve, reject) => {
+        //     console.log(`renderSvg - render svg`, { x });
+
+        //     svgexport.render([{
+        //         input: [x, `png`, 100, `32:32`],
+        //         output: [x + `.png`],
+        //     }], () => {
+        //         resolve();
+        //     });
+        // });
+    }));
+
+};
