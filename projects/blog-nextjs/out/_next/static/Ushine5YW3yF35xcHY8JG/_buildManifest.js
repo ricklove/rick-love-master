e4c5fce91bd5e98e85c828903adf54cdc750745c
Rick Love <rick@toldpro.com>
2021-08-24 00:04:37 -0500

    Test Build nextjs

---
+++ projects/blog-nextjs/out/_next/static/Ushine5YW3yF35xcHY8JG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8cf1921a7d1402aabd30.js"],"/_error":["static/chunks/pages/_error-7d11879d3155c74af961.js"],"/test":["static/chunks/pages/test-6c4c519f4652ab136999.js"],sortedPages:["/","/_app","/_error","/test"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
