324f3e71737d3ecbc91d5779a83274536c245234
Rick Love <rick@toldpro.com>
2021-08-24 17:00:41 -0500

    Use react eslint mixin

--- config/eslint/rules.js
+++ config/eslint/rules.js
@@ -1,14 +1,21 @@
 // This is a workaround for https://github.com/eslint/eslint/issues/3458
-require(`@rushstack/eslint-config/patch/modern-module-resolution`);
+require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: [`@rushstack/eslint-config/profile/web-app`],
+  extends: ['@rushstack/eslint-config/profile/web-app', '@rushstack/eslint-config/mixins/react'],
   parserOptions: { tsconfigRootDir: __dirname },
+  settings: {
+    react: {
+      version: '17.0',
+    },
+  },
   plugins: ['import-quotes'],
   ignorePatterns: ['lib', '.eslintrc.js'],
+
   rules: {
     '@rushstack/typedef-var': 'off',
     '@typescript-eslint/naming-convention': 'off',
+
     // Backticks Everywhere (except jsx and imports)
     quotes: 'off',
     '@typescript-eslint/quotes': ['error', 'backtick'],
