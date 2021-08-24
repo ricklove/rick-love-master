975104bb90653ba6a7461e49f10d132b589ce043
Rick Love <rick@toldpro.com>
2021-08-23 23:33:24 -0500

    Almost working, but nextjs confused with ts module

--- packages/example-local/src/example-code.ts
+++ packages/example-local/src/example-code.ts
@@ -1,10 +1,12 @@
-export const exampleValue6 = {
+export const exampleValue7 = {
   example3: 'Here it is nice!',
 
   run2: () => {
     const a = `I just want to test some code formatting!`;
     const b = ' This is not formatted!';
+    let c: string;
+    c = b + a;
 
-    return { a, b, ok: true };
+    return { a, b, c, ok: true };
   },
 };
--- projects/blog-nextjs/pages/test.tsx
+++ projects/blog-nextjs/pages/test.tsx
@@ -1,9 +1,9 @@
-import { exampleValue6 } from '@ricklove/example-local';
+import { exampleValue7 } from '@ricklove/example-local';
 
 export default () => {
   return (
     <>
-      <div>{`${exampleValue6.example3}: ${exampleValue6.run2().a}`}</div>
+      <div>{`${exampleValue7.example3}: ${exampleValue7.run2().c}`}</div>
     </>
   );
 };
