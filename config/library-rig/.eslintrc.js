d4b1e0dabb2f8565d80e6eb663dd32733ce93376
Rick Love <rick@toldpro.com>
2021-08-23 22:34:48 -0500

    Doc eslint

--- config/library-rig/.eslintrc.js
+++ config/library-rig/.eslintrc.js
@@ -2,8 +2,11 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
 
 module.exports = {
-  extends: ['@rushstack/eslint-config/profile/node'],
+  extends: ['@rushstack/eslint-config/profile/web-app'],
   parserOptions: { tsconfigRootDir: __dirname },
+
+  // https://github.com/microsoft/rushstack/blob/master/stack/eslint-config/profile/_common.js
+  // https://github.com/microsoft/rushstack/blob/master/stack/eslint-plugin
   rules: {
     '@rushstack/typedef-var': 'off',
     '@typescript-eslint/naming-convention': 'off',
