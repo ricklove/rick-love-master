0791fa7740bdba1b8ee5548e3b7b2206d5b06a4f
Rick Love <rick@toldpro.com>
2021-08-28 16:36:03 -0500

    Test build

--- projects/web/ricklove_me-build/src/build.ts
+++ projects/web/ricklove_me-build/src/build.ts
@@ -5,29 +5,6 @@ const fs = fsRaw.promises;
 const sourcePath = path.resolve(__dirname, `../../ricklove_me-content/src/pages`);
 const destPath = path.resolve(__dirname, `../../ricklove_me/pages`);
 
-export const createIndex = async () => {
-  const pageFiles = await fs.readdir(sourcePath);
-  const indexContent = pageFiles
-    .map((f) => {
-      const pageName = path.parse(f).name;
-      // export { page as page_posts } from './src/pages/posts';
-      return `export { page as page_${pageName} } from './src/pages/${pageName}';\n`;
-    })
-    .join(``);
-
-  const indexFilePath = path.resolve(sourcePath, `../../index.ts`);
-
-  const oldIndexFileContent = fsRaw.existsSync(indexFilePath)
-    ? await fs.readFile(indexFilePath, { encoding: `utf-8` })
-    : null;
-  if (oldIndexFileContent === indexContent) {
-    return;
-  }
-
-  console.log(`createIndex - creating index file`, { indexFilePath });
-  await fs.writeFile(indexFilePath, indexContent);
-};
-
 export const createPages = async () => {
   const pageFiles = await fs.readdir(sourcePath);
 
@@ -51,7 +28,6 @@ export default Page;
 };
 
 const run = async () => {
-  await createIndex();
   await createPages();
 };
 
---
+++ projects/web/ricklove_me-content/scripts/build.ts
@@ -0,0 +1,30 @@
+import fsRaw from 'fs';
+import path from 'path';
+const fs = fsRaw.promises;
+
+const sourcePath = path.resolve(__dirname, `../src/pages`);
+
+export const createIndex = async () => {
+  const pageFiles = await fs.readdir(sourcePath);
+  const indexContent = pageFiles
+    .map((f) => {
+      const pageName = path.parse(f).name;
+      // export { page as page_posts } from './src/pages/posts';
+      return `export { page as page_${pageName} } from './src/pages/${pageName}';\n`;
+    })
+    .join(``);
+
+  const indexFilePath = path.resolve(sourcePath, `../../index.ts`);
+
+  const oldIndexFileContent = fsRaw.existsSync(indexFilePath)
+    ? await fs.readFile(indexFilePath, { encoding: `utf-8` })
+    : null;
+  if (oldIndexFileContent === indexContent) {
+    return;
+  }
+
+  console.log(`createIndex - creating index file`, { indexFilePath });
+  await fs.writeFile(indexFilePath, indexContent);
+};
+
+void createIndex();
