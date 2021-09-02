6032df13b1f49b40d05254f8ed5167d0f94db287
Rick Love <rick@toldpro.com>
2021-09-02 13:30:05 -0500

    Fix auto imports command

--- tools/vscode/auto-imports-fix/src/extension.ts
+++ tools/vscode/auto-imports-fix/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { fixAutoImports } from './fix-auto-imports';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,11 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(`auto-imports-fix.helloWorld`, () => {
+  const disposable = vscode.commands.registerCommand(`auto-imports-fix.fixAutoImports`, () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    vscode.window.showInformationMessage(`Hello World from auto-imports-fix!`);
+    fixAutoImports(context);
   });
 
   context.subscriptions.push(disposable);
