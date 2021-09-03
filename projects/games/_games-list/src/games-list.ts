9fe50d8d86d2adc386ba2435e47c4737dcc05e1a
Rick Love <rick@toldpro.com>
2021-09-02 21:48:23 -0500

    Include educational games

--- projects/games/_games-list/src/games-list.ts
+++ projects/games/_games-list/src/games-list.ts
@@ -1,24 +1,14 @@
 import { DoodleConfig } from '@ricklove/doodle-client';
+import { educationalGamesList, educationalGameUtils } from '@ricklove/educational-games';
 
 export type GameConfig = DoodleConfig;
 export type GameComponent = (props: { config: GameConfig }) => JSX.Element;
 export const gamesList: { name: string; load: () => Promise<GameComponent> }[] = [
-  // { name: `multiples-counting`, load: async () => (await import(`./multiples-counting`)).EducationalGame_MultiplesCounting },
-  // { name: `multiples-counting-words`, load: async () => (await import(`./multiples-counting-words`)).EducationalGame_MultiplesCountingWords },
-  // { name: `multiples-monster`, load: async () => (await import(`./multiples-monster`)).EducationalGame_MultiplesMonster },
-  // { name: `multiples-snake`, load: async () => (await import(`./multiples-snake`)).EducationalGame_MultiplesSnake },
-  // { name: `multiples-large-board`, load: async () => (await import(`./multiples-large-board`)).EducationalGame_MultiplesLargeBoard },
-  // { name: `multiples-star-blast`, load: async () => (await import(`./star-blast-sideways`)).EducationalGame_StarBlastSideways_Multiples },
-  // { name: `spanish-star-blast`, load: async () => (await import(`./star-blast-sideways-with-subjects/star-blast-spanish`)).EducationalGame_StarBlastSideways_Spanish },
-  // { name: `spelling-star-blast`, load: async () => (await import(`./star-blast-sideways-with-subjects/star-blast-spelling`)).EducationalGame_StarBlastSideways_Spelling },
-  // { name: `doodle-spelling`, load: async () => (await import(`./doodle-subjects/doodle-spelling`)).EducationalGame_Doodle_Spelling },
+  ...educationalGamesList,
   { name: `doodle-browser`, load: async () => (await import(`@ricklove/doodle-client`)).DoodleBrowser },
   { name: `doodle-party`, load: async () => (await import(`@ricklove/doodle-client`)).DoodlePartyView },
 ];
 
-// export const educationalGameUtils = {
-//   progressGame: {
-//     name: `progressGame`,
-//     load: async () => (await import(`./progress-games/progress-game`)).ProgressGameView,
-//   },
-// };
+export const gameUtils = {
+  ...educationalGameUtils,
+};
