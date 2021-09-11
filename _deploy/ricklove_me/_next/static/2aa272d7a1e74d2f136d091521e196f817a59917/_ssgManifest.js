d6b935a511f3d17e10cbaab1359173f30ea38620
Rick Love <rick@toldpro.com>
2021-09-11 08:29:33 -0500

    Rebuild web

---
+++ _deploy/ricklove_me/_next/static/2aa272d7a1e74d2f136d091521e196f817a59917/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002F","\u002F404","\u002Fgames","\u002Flessons","\u002Fgames\u002F[name]","\u002Fblog\u002F[...slug]","\u002Flessons\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
