07f33267e0ad7100f439bdeb47072132bb13b16c
Rick Love <rick@toldpro.com>
2021-09-14 20:01:10 -0500

    Simplify artwork list

--- projects/art/_build/src/artworkList.ts
+++ projects/art/_build/src/artworkList.ts
@@ -1,14 +1,18 @@
-import { Artwork, ArtworkMetadata } from '@ricklove/art-common';
 import { ArtworkComponentOptions, createArtworkComponentLoader } from '@ricklove/art-components';
 import { metadata as metadata_circles } from '@ricklove/artwork-circles';
 
-const _artworkList: { metadata: ArtworkMetadata; loadArtwork: () => Promise<Artwork> }[] = [
-  { metadata: metadata_circles, loadArtwork: async () => (await import(`@ricklove/artwork-circles`)).artwork },
+const _artworkList = [
+  { metadata: metadata_circles, importModule: async () => await import(`@ricklove/artwork-circles`) },
 ];
 
-export const artworkList = _artworkList.map((x) => ({
-  key: x.metadata.key,
-  metadata: x.metadata,
-  load: async (tokenId: string, options?: ArtworkComponentOptions) =>
-    await createArtworkComponentLoader(await x.loadArtwork(), tokenId, options),
-}));
+export const artworkList = _artworkList
+  .map((x) => ({
+    ...x,
+    loadArtwork: async () => (await x.importModule()).artwork,
+  }))
+  .map((x) => ({
+    key: x.metadata.key,
+    metadata: x.metadata,
+    load: async (tokenId: string, options?: ArtworkComponentOptions) =>
+      await createArtworkComponentLoader(await x.loadArtwork(), tokenId, options),
+  }));
