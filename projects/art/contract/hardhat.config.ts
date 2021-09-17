5dd5018b00165d63d4a3df071fc52d446067ce3b
Rick Love <rick@toldpro.com>
2021-09-17 14:06:38 -0500

    Run tests

--- projects/art/contract/hardhat.config.ts
+++ projects/art/contract/hardhat.config.ts
@@ -1,8 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -13,9 +16,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
+const config: HardhatUserConfig = {
+  // Your type-safe config goes here
+  solidity: '0.8.7',
 };
+
+export default config;
