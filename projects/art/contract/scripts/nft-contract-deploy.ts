76754eefb15425b82c7aed63ebfc4e62b004cfbe
Rick Love <rick@toldpro.com>
2021-09-17 21:04:30 -0500

    Cleanup

--- projects/art/contract/scripts/nft-contract-deploy.ts
+++ projects/art/contract/scripts/nft-contract-deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
+  const NftContract = await hre.ethers.getContractFactory('NftContract');
+  const contract = await NftContract.deploy();
 
-  await greeter.deployed();
+  await contract.deployed();
 
-  console.log('Greeter deployed to:', greeter.address);
+  console.log('NftContract deployed to:', contract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
