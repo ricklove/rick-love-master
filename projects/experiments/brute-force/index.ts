0499f7c12f3cd416d0eff667f5311761b7f64486
Rick Love <rick@toldpro.com>
2021-09-30 22:39:05 -0500

    Cleanup brute force

---
+++ projects/experiments/brute-force/index.ts
@@ -0,0 +1,1 @@
+export * from './src/the-greats-answer01';
---
+++ projects/experiments/brute-force/src/cli.ts
@@ -0,0 +1,4 @@
+import { bruteForce_theGreats_answer01 } from './the-greats-answer01';
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bruteForce_theGreats_answer01();
