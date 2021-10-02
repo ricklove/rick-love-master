79484d30019180d3932d9e49aac1c301dc601a4d
Rick Love <rick@toldpro.com>
2021-10-01 23:17:59 -0500

    Create sample shader

--- projects/art/artwork/hashing/src/artwork.ts
+++ projects/art/artwork/hashing/src/artwork.ts
@@ -1,4 +1,4 @@
-import { ArtWork_p5, createRandomGenerator } from '@ricklove/art-common';
+import { ArtWork_p5, p5Types } from '@ricklove/art-common';
 import { metadata } from './metadata';
 
 export const artwork: ArtWork_p5 = {
@@ -7,41 +7,68 @@ export const artwork: ArtWork_p5 = {
   config: {
     webgl: true,
   },
-  render: (tokenId) => {
-    const { random } = createRandomGenerator(tokenId);
+  render: (_tokenId) => {
+    // const { random } = createRandomGenerator(tokenId);
 
-    const { a, b, c } = {
-      a: 1 + Math.floor(57 * random()),
-      b: 1 + Math.floor(213 * random()),
-      c: 1 + Math.floor(115 * random()),
-    };
-    const { cr, cg, cb, ca } = {
-      cr: Math.floor(25 + 230 * random()),
-      cg: Math.floor(25 + 230 * random()),
-      cb: Math.floor(25 + 230 * random()),
-      ca: Math.floor(25 + 25 * random()),
-    };
+    // https://github.com/phusion/node-sha3/blob/main/src/sponge/index.js
+
+    // Create kernel
+
+    // const { a, b, c } = {
+    //   a: 1 + Math.floor(57 * random()),
+    //   b: 1 + Math.floor(213 * random()),
+    //   c: 1 + Math.floor(115 * random()),
+    // };
+    // const { cr, cg, cb, ca } = {
+    //   cr: Math.floor(25 + 230 * random()),
+    //   cg: Math.floor(25 + 230 * random()),
+    //   cb: Math.floor(25 + 230 * random()),
+    //   ca: Math.floor(25 + 25 * random()),
+    // };
+
+    // the 'varying's are shared between both vertex & fragment shaders
+    const varying = `precision highp float; varying vec2 vPos;`;
+
+    // the vertex shader is called for each vertex
+    const vs = varying + `attribute vec3 aPosition;` + `void main() { vPos = (gl_Position = vec4(aPosition,1.0)).xy; }`;
+
+    // the fragment shader is called for each pixel
+    const fs =
+      varying +
+      `uniform vec2 p;` +
+      `uniform float r;` +
+      `const int I = 500;` +
+      `void main() {` +
+      `  vec2 c = p + vPos * r, z = c;` +
+      `  float n = 0.0;` +
+      `  for (int i = I; i > 0; i --) {` +
+      `    if(z.x*z.x+z.y*z.y > 4.0) {` +
+      `      n = float(i)/float(I);` +
+      `      break;` +
+      `    }` +
+      `    z = vec2(z.x*z.x-z.y*z.y, 2.0*z.x*z.y) + c;` +
+      `  }` +
+      `  gl_FragColor = vec4(0.5-cos(n*17.0)/2.0,0.5-cos(n*13.0)/2.0,0.5-cos(n*23.0)/2.0,1.0);` +
+      `}`;
+
+    let mandel = null as null | p5Types.Shader;
 
     return {
+      setup: (p5) => {
+        mandel = p5.createShader(vs, fs);
+        p5.shader(mandel);
+        p5.noStroke();
+        // 'p' is the center point of the Mandelbrot image
+        mandel.setUniform(`p`, [-0.74364388703, 0.13182590421]);
+      },
       draw: (p5, { time, canvasSize }) => {
-        const h = 200;
-        const scale = canvasSize.x / 400;
-
-        p5.background(0);
-        p5.translate(-canvasSize.x * 0.5, -canvasSize.y * 0.5);
-        p5.scale(scale);
-
-        for (let i = 0; i < 10; i++) {
-          const color = p5.color((cr * i) % 255, (cg * i) % 255, (cb * i) % 255, ca);
-          p5.noFill();
-          p5.stroke(color);
-          for (let j = 0; j < 36; j++) {
-            p5.circle(h - a / 2 + (j % a), h - b / 2 + (j % b), h * 1.35 - ((i * 5) % c));
-          }
-          p5.translate(h, h);
-          p5.rotate(a + b + c + (time * 600 + i) * 0.0005);
-          p5.translate(-h, -h);
+        if (!mandel) {
+          return;
         }
+
+        // 'r' is the size of the image in Mandelbrot-space
+        mandel.setUniform(`r`, 1.5 * p5.exp(-6.5 * (1 + p5.sin(p5.millis() / 2000))));
+        p5.quad(-1, -1, 1, -1, 1, 1, -1, 1);
       },
     };
   },
