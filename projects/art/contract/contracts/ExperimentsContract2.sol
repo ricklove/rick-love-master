a82c26d2428a11951c0a306e72eb00ca281f4500
Rick Love <rick@toldpro.com>
2021-12-22 17:24:21 -0600

    delta: simple add

--- projects/art/contract/contracts/ExperimentsContract2.sol
+++ projects/art/contract/contracts/ExperimentsContract2.sol
@@ -11,32 +11,23 @@ contract ExperimentsContract2 {
 
         assembly {
             function getNextData() -> data { 
-                data := 0xA3
-            }
-
-            function getNextData2() -> data { 
-                let p := mload(0xB3)
+                let p := mload(0xA3)
                 data := mload(p)
-                mstore(0xB5, add(p, 1))
+                mstore(0xA3, add(p, 1))
             }
 
-            function getNextData3() -> data { 
-                let p := mload(0xC3)
-                data := add(mload(p), getNextData())
-                mstore(0xC5, add(p, 1))
-            }
-
-            let a := add(0xA6,getNextData())
-            let b := getNextData2()
-            let c := getNextData3()
-            let d := add(a,add(b,add(c,getNextData())))
+            let a := getNextData()
+            let b := getNextData()
+            let c := getNextData()
+            let d := getNextData()
             let e := getNextData()
 
             let x := d
-            for { let i := 0 } lt(i, 0x100) { i := add(i, 0x20) } {
-                x := add(x, getNextData3())
-                x := add(x, 0xD3)
-            }
+            x := add(x,a)
+            x := add(x,b)
+            x := add(x,c)
+            x := add(x,d)
+            x := add(x,e)
 
             output := x
         }
