62e0e875687b9cae8470c7a69cdbacdc64e4a3ea
Rick Love <rick@toldpro.com>
2022-09-02 07:44:19 -0500

    Extract text

--- projects/bible/bible-cli-tools/src/bible-textual-analysis/extract-text-features.ts
+++ projects/bible/bible-cli-tools/src/bible-textual-analysis/extract-text-features.ts
@@ -4,204 +4,209 @@ import path from 'path';
 export const extractBibleTextFeatures = async (sourceFilePath: string, destDirPath: string) => {
   const sourceText = await fs.readFile(sourceFilePath, { encoding: `utf-8` });
 
-  const books = `
-1. Genesis
-2. Exodus
-3. Leviticus
-4. Numbers
-5. Deuteronomy
-6. Joshua
-7. Judges
-8. Ruth
-9. 1 Samuel
-10. 2 Samuel
-11. 1 Kings
-12. 2 Kings
-13. 1 Chronicles
-14. 2 Chronicles
-15. Ezra
-16. Nehemiah
-17. Esther
-18. Job
-19. Psalms
-20. Proverbs
-21. Ecclesiastes
-22. Song of Songs
-23. Isaiah
-24. Jeremiah
-25. Lamentations
-26. Ezekiel
-27. Daniel
-28. Hosea
-29. Joel
-30. Amos
-31. Obadiah
-32. Jonah
-33. Micah
-34. Nahum
-35. Habakkuk
-36. Zephaniah
-37. Haggai
-38. Zechariah
-39. Malachi
-40. Matthew
-41. Mark
-42. Luke
-43. John
-44. Acts
-45. Romans
-46. 1 Corinthians
-47. 2 Corinthians
-48. Galatians
-49. Ephesians
-50. Philippians
-51. Colossians
-52. 1 Thessalonians
-53. 2 Thessalonians
-54. 1 Timothy
-55. 2 Timothy
-56. Titus
-57. Philemon
-58. Hebrews
-59. James
-60. 1 Peter
-61. 2 Peter
-62. 1 John
-63. 2 John
-64. 3 John
-65. Jude
-66. Revelation
-  `
-    .split(`\n`)
-    .map((x) => x.trim())
-    .filter((x) => x)
-    .map((l) => l.split(`.`))
-    .map(([number, name]) => ({
-      number,
-      name,
-    }));
+  const parseBooks = () => {
+    // Example Format:
+    // 1. Genesis
+    // 1.1. Chapter 1
+    // 1
+    //
+    // (The Creation of the World)
+    // In the beginning, God created the heavens
+    // and the earth.
+    // 2
+    //
+    // The earth was without form and void, and
+    // darkness was over the face of the deep. And
+    // the Spirit of God was hovering over the face
+    // of the waters.
+    // 3
+    //
+    // And God said, "Let there be light," and there
+    // was light.
 
-  const booksStartIndexes = books.map((b) => {
-    const iBook = sourceText.lastIndexOf(`${b.number}. ${b.name}`);
-    return {
-      ...b,
-      iStart: iBook,
+    // 40.5. Chapter 5
+    // 1
+    //
+    // (The Sermon on the Mount)
+    // Seeing the crowds, he went up on the
+    // mountain, and when he sat down, his
+    // disciples came to him.
+    // 2(The Beatitudes)
+    // And he opened his mouth and taught them,
+    // saying:
+
+    const allLines = sourceText.split(`\n`).map((x) => x.trim());
+    const iFirstLine = allLines.lastIndexOf(`1. Genesis`);
+    const lines = allLines.slice(iFirstLine);
+    console.log(`extractBibleTextFeatures.parseBooks`, { iFirstLine, firstLines: lines.slice(0, 10) });
+
+    let curBook = { bookName: ``, bookNumber: 1 };
+    let curChapter = 0;
+    let curVerse = 0;
+    let curSection = {
+      heading: ``,
+      start: {
+        chapterNumber: 1,
+        verseNumber: 1,
+      },
+      end: {
+        chapterNumber: 1,
+        verseNumber: 1,
+      },
     };
-  });
+    let curVerseLines = [] as string[];
 
-  const bookEntries = booksStartIndexes
-    .map((b, i) => {
-      const iEndExclusive = booksStartIndexes[i + 1]?.iStart ?? sourceText.length;
-      return {
-        ...b,
-        iEnd: iEndExclusive,
-      };
-    })
-    .map((b) => {
-      const bookText = sourceText.substring(b.iStart, b.iEnd);
-
-      // Example Format:
-      // 1. Genesis
-      // 1.1. Chapter 1
-      // 1
-      //
-      // (The Creation of the World)
-      // In the beginning, God created the heavens
-      // and the earth.
-      // 2
-      //
-      // The earth was without form and void, and
-      // darkness was over the face of the deep. And
-      // the Spirit of God was hovering over the face
-      // of the waters.
-      // 3
-      //
-      // And God said, "Let there be light," and there
-      // was light.
-
-      const lines = bookText.split(`\n`).slice(1);
-
-      let nChapter = 0;
-      let nVerse = 0;
-      let sectionStart = { heading: ``, chapterNumber: 1, verseNumber: 1 };
-      let verseLines = [] as string[];
-
-      const verses = [] as {
-        text: string;
+    let verses = [] as {
+      text: string;
+      chapterNumber: number;
+      verseNumber: number;
+    }[];
+    let sections = [] as {
+      heading: string;
+      start: {
         chapterNumber: number;
         verseNumber: number;
-      }[];
-      const sections = [] as {
-        heading: string;
-        start: {
-          chapterNumber: number;
-          verseNumber: number;
-        };
-        end: {
-          chapterNumber: number;
-          verseNumber: number;
-        };
-      }[];
-
-      const completeVerse = () => {
-        verses.push({
-          text: verseLines.join(`\n`),
-          chapterNumber: nChapter,
-          verseNumber: nVerse,
-        });
-
-        verseLines = [];
       };
-
-      const completeSection = () => {
-        sections.push({
-          heading: sectionStart.heading,
-          start: {
-            chapterNumber: sectionStart.chapterNumber,
-            verseNumber: sectionStart.verseNumber,
-          },
-          end: {
-            chapterNumber: nChapter,
-            verseNumber: nVerse,
-          },
-        });
+      end: {
+        chapterNumber: number;
+        verseNumber: number;
       };
+    }[];
+    const books = [] as {
+      bookName: string;
+      bookNumber: number;
+      verses: typeof verses;
+      sections: typeof sections;
+    }[];
 
-      for (const l of lines) {
-        const mChapter = l.match(/\d+\.\d+ Chapter (\d+)/);
-        if (mChapter) {
-          completeVerse();
-          nChapter = Number(mChapter[1]);
-          return;
-        }
+    const completeVerse = () => {
+      if (curVerse < 1) {
+        return;
+      }
+      verses.push({
+        text: curVerseLines.join(`\n`),
+        chapterNumber: curChapter,
+        verseNumber: curVerse,
+      });
 
-        const mVerse = l.match(/^(\d+)$/);
-        if (mVerse) {
-          completeVerse();
-          nVerse = Number(mVerse[1]);
-          return;
-        }
+      curVerseLines = [];
+    };
 
-        const mSection = l.match(/^\((.*)\)$/);
-        if (mSection) {
-          completeSection();
-          sectionStart = { heading: mSection[1], chapterNumber: nChapter, verseNumber: nVerse };
-          return;
-        }
-
-        verseLines.push(l);
+    const completeSection = () => {
+      if (!curSection.heading) {
+        return;
       }
 
-      completeVerse();
-      completeSection();
-
-      return {
-        bookName: b.name,
-        bookNumber: b.number,
-        verses,
-        sections,
+      sections.push(curSection);
+      curSection = {
+        heading: ``,
+        start: {
+          chapterNumber: 1,
+          verseNumber: 1,
+        },
+        end: {
+          chapterNumber: 1,
+          verseNumber: 1,
+        },
       };
-    });
+    };
 
+    const completeBook = () => {
+      if (!verses.length) {
+        return;
+      }
+      books.push({
+        bookName: curBook.bookName,
+        bookNumber: curBook.bookNumber,
+        sections,
+        verses,
+      });
+
+      verses = [];
+      sections = [];
+      curSection = {
+        heading: ``,
+        start: {
+          chapterNumber: 1,
+          verseNumber: 1,
+        },
+        end: {
+          chapterNumber: 1,
+          verseNumber: 1,
+        },
+      };
+    };
+
+    for (const l of lines) {
+      const mChapter = l.match(/^\d+\.\d+\. Chapter (\d+)$/);
+      if (mChapter) {
+        completeVerse();
+        curChapter = Number(mChapter[1]);
+        continue;
+      }
+
+      const mBook = l.match(/^(\d+)\. (.+)$/);
+      if (mBook) {
+        // console.log(`mBook`, { mBook, m0: mBook[0], m1: mBook[1], m2: mBook[2] });
+
+        completeVerse();
+        completeSection();
+        completeBook();
+        curBook = { bookNumber: Number(mBook[1]), bookName: mBook[2] };
+        continue;
+      }
+
+      const mVerseWithSection = l.match(/^(\d+)\((.*)\)$/);
+      if (mVerseWithSection) {
+        completeVerse();
+        curVerse = Number(mVerseWithSection[1]);
+        completeSection();
+        curSection = {
+          heading: mVerseWithSection[2],
+          start: { chapterNumber: curChapter, verseNumber: curVerse },
+          end: { chapterNumber: curChapter, verseNumber: curVerse },
+        };
+        continue;
+      }
+
+      const mVerse = l.match(/^(\d+)$/);
+      if (mVerse) {
+        completeVerse();
+        curVerse = Number(mVerse[1]);
+        continue;
+      }
+
+      const mSection = l.match(/^\((.*)\)$/);
+      if (mSection) {
+        completeSection();
+        curSection = {
+          heading: mSection[1],
+          start: { chapterNumber: curChapter, verseNumber: curVerse },
+          end: { chapterNumber: curChapter, verseNumber: curVerse },
+        };
+        continue;
+      }
+
+      if (!l.trim()) {
+        curVerseLines.push(``);
+        continue;
+      }
+
+      curSection.end = { chapterNumber: curChapter, verseNumber: curVerse };
+      curVerseLines.push(l);
+    }
+
+    completeVerse();
+    completeSection();
+    completeBook();
+
+    return {
+      books,
+    };
+  };
+
+  const bookEntries = parseBooks();
+
+  await fs.mkdir(destDirPath, { recursive: true });
   await fs.writeFile(path.join(destDirPath, `./books-all.json`), JSON.stringify(bookEntries, null, 2));
 };
--- projects/bible/bible-cli-tools/src/cli.ts
+++ projects/bible/bible-cli-tools/src/cli.ts
@@ -3,7 +3,7 @@ import { extractBibleTextFeatures } from './bible-textual-analysis/extract-text-
 
 export const run = async () => {
   // This requires a text source of the entire bible which is not included in the repo because of copyright restrictions
-  await extractBibleTextFeatures(path.resolve(`./data/esv-bible-raw-analysis-text.txt`), path.resolve(`./output/`));
+  await extractBibleTextFeatures(path.resolve(`./data/esv-bible-raw-analysis-text.txt`), path.resolve(`./output/esv/`));
 };
 
 run().catch((err) => console.error(err));
