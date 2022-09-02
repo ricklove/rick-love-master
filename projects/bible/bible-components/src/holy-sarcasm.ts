c98b9a0fcb2aef4c6ab965c7ce6d775845923e7c
Rick Love <rick@toldpro.com>
2022-09-02 15:41:31 -0500

    Change sarcasm

--- projects/bible/bible-components/src/holy-sarcasm.ts
+++ projects/bible/bible-components/src/holy-sarcasm.ts
@@ -1,10 +1,16 @@
 export const getHolySarcasticPhrase = (selfRighteousnessLevel: number) => {
   if (selfRighteousnessLevel >= 1.0) {
-    return `Thou art 100x more righeteous than we art!`;
+    return `You don't think that your poop don't stink!`;
+  }
+  if (selfRighteousnessLevel >= 0.75) {
+    return `Almost holy!`;
+  }
+  if (selfRighteousnessLevel >= 0.5) {
+    return `You are holy as a pair of old underwear!`;
   }
   if (selfRighteousnessLevel >= 0.25) {
     return `You art not that holy!`;
   }
 
-  return `Unclean!`;
+  return `I am 100x more righeteous than thou art!`;
 };
