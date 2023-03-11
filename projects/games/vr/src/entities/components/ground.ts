be595221b3f82e1bdedd59d130d824c4f8af2a37
Rick Love <rick@toldpro.com>
2023-03-10 19:23:34 -0600

    FIX Ground height

--- projects/games/vr/src/entities/components/ground.ts
+++ projects/games/vr/src/entities/components/ground.ts
@@ -1,3 +1,4 @@
+import { logger } from '../../utils/logger';
 import { defineComponent, Vector3 } from '../core';
 
 export type EntityAdjustToGround = {
@@ -29,6 +30,7 @@ export const EntityAdjustToGround = defineComponent<EntityAdjustToGround>()
       const yGround = EntityGround.getWorldHeightAtPoint(ground, pos.x, pos.z);
       const { minGroundHeight, maxGroundHeight } = entity.adjustToGround;
 
+      logger.log(`adjustToGround`, { y, yGround });
       if (minGroundHeight != null && y < yGround + minGroundHeight) {
         pos.setY(yGround + minGroundHeight);
       }
@@ -69,21 +71,25 @@ export const EntityGround = defineComponent<EntityGround>()
 
       const edgeCount = segmentCount + 1;
       const iCenter = 0.5 * segmentCount;
+      const jCenter = 0.5 * segmentCount;
 
       const grid = new Float32Array(edgeCount * edgeCount);
       for (let i = 0; i < edgeCount; i++) {
         for (let j = 0; j < edgeCount; j++) {
+          // grid values should be between 0-1
+
           // Rolling Mountains
-          grid[j * edgeCount + i] = Math.cos(0.73 * i) * Math.sin(0.31 * j);
+          grid[j * edgeCount + i] = 0.5 + 0.5 * (Math.cos(0.73 * i) * Math.sin(0.31 * j));
 
           // // Sharp Mountains
-          // grid[j * edgeCount + i] = Math.sin(j * edgeCount + i);
+          // grid[j * edgeCount + i] = 0.5 +0.5*(Math.sin(j * edgeCount + i));
 
           // Wavy
-          // grid[j * edgeCount + i] = Math.sin(i);
+          // grid[j * edgeCount + i] = 0.5 +0.5*(Math.sin(i));
 
           // Plane
-          // grid[j * edgeCount + i] = (i - iCenter) / edgeCount;
+          // grid[j * edgeCount + i] = j / segmentCount;
+          // grid[j * edgeCount + i] = i / segmentCount;
         }
       }
 
@@ -128,13 +134,14 @@ const getHeightAtPoint = (ground: EntityGround, x: number, z: number): number =>
   // i==0 is far left edge
 
   const i = (x - position.x) / segmentSize + iCenter;
-  const j = (z - position.z) / segmentSize + jCenter;
+  const jInv = (z - position.z) / segmentSize + jCenter;
+  const j = segmentCount - jInv;
 
   // eslint-disable-next-line no-bitwise
-  const l = i | 0;
+  const l = Math.max(0, Math.min(edgeCount - 2, i | 0));
   const r = l + 1;
   // eslint-disable-next-line no-bitwise
-  const t = j | 0;
+  const t = Math.max(0, Math.min(edgeCount - 2, j | 0));
   const b = t + 1;
 
   const tl = grid[t * edgeCount + l] || 0;
@@ -142,47 +149,61 @@ const getHeightAtPoint = (ground: EntityGround, x: number, z: number): number =>
   const bl = grid[b * edgeCount + l] || 0;
   const br = grid[b * edgeCount + r] || 0;
 
-  const bRatio = t === j ? 0 : j % 1;
-  const rRatio = l === i ? 0 : i % 1;
+  const bRatio = t === j ? 0 : j < 0 ? 0 : j > segmentCount ? 1 : j % 1;
+  const rRatio = l === i ? 0 : i < 0 ? 0 : i > segmentCount ? 1 : i % 1;
   const tRatio = 1 - bRatio;
   const lRatio = 1 - rRatio;
 
+  const tlRatio = tRatio * lRatio;
+  const trRatio = tRatio * rRatio;
+  const blRatio = bRatio * lRatio;
+  const brRatio = bRatio * rRatio;
+
   // prettier-ignore
   const ave = 0
-    + tl * tRatio * lRatio
-    + tr * tRatio * rRatio
-    + bl * bRatio * lRatio
-    + br * bRatio * rRatio;
+    + tl * tlRatio
+    + tr * trRatio
+    + bl * blRatio
+    + br * brRatio;
 
   const y = ave * (maxHeight - minHeight) + minHeight + position.y;
 
-  // logger.log(`h`, {
-  //   x: x.toFixed(3),
-  //   z: z.toFixed(3),
-  //   i: i.toFixed(3),
-  //   j: j.toFixed(3),
-  // });
-  // logger.log(`h`, {
-  //   t: t,
-  //   l: l,
-  //   b: b,
-  //   r: r,
-  // });
-  // logger.log(`h`, {
-  //   tR: tRatio.toFixed(3),
-  //   lR: lRatio.toFixed(3),
-  //   bR: bRatio.toFixed(3),
-  //   rR: rRatio.toFixed(3),
-  // });
-  // logger.log(`h`, {
-  //   tl: tl.toFixed(3),
-  //   tr: tr.toFixed(3),
-  //   bl: bl.toFixed(3),
-  //   br: br.toFixed(3),
-  // });
-  // logger.log(`h`, {
-  //   y: y.toFixed(3),
-  //   ave: ave.toFixed(3),
-  // });
+  logger.log(`h`, {
+    x: x.toFixed(3),
+    z: z.toFixed(3),
+    i: i.toFixed(3),
+    j: j.toFixed(3),
+  });
+  logger.log(`h`, {
+    t: t,
+    l: l,
+    b: b,
+    r: r,
+  });
+  logger.log(`h`, {
+    tR: tRatio.toFixed(3),
+    lR: lRatio.toFixed(3),
+    bR: bRatio.toFixed(3),
+    rR: rRatio.toFixed(3),
+  });
+  logger.log(`h`, {
+    tl: tl.toFixed(3),
+    tr: tr.toFixed(3),
+    bl: bl.toFixed(3),
+    br: br.toFixed(3),
+  });
+  logger.log(`h`, {
+    tlR: tlRatio.toFixed(3),
+    trR: trRatio.toFixed(3),
+    blR: blRatio.toFixed(3),
+    brR: brRatio.toFixed(3),
+  });
+  logger.log(`h`, {
+    y: y.toFixed(3),
+    ave: ave.toFixed(3),
+    gy: position.y.toFixed(3),
+    min: minHeight.toFixed(3),
+    max: maxHeight.toFixed(3),
+  });
   return y;
 };
--- projects/games/vr/src/utils/logger.tsx
+++ projects/games/vr/src/utils/logger.tsx
@@ -2,10 +2,30 @@ import React, { useLayoutEffect, useState } from 'react';
 import { Text } from '@react-three/drei';
 import { filter, interval, map } from 'rxjs';
 
+const formatDetails = (details?: unknown): string => {
+  // eslint-disable-next-line eqeqeq
+  if (details == undefined) {
+    return ``;
+  }
+  if (Array.isArray(details)) {
+    return details.map((x) => formatDetails(x)).join(`,`);
+  }
+  if (typeof details === `object`) {
+    return `{${Object.entries(details)
+      .map(([k, v]) => `${k}:${formatDetails(v)}`)
+      .join(`,`)}}`;
+  }
+  if (typeof details === `number`) {
+    return details.toFixed(2);
+  }
+  return String(details);
+};
+
 const logState = [] as string[];
 const log = (message: string, details?: unknown) => {
-  console.log(message, details);
-  logState.unshift(`${logState.length} ${Date.now() % 1000000}: ${message} ${details ? JSON.stringify(details) : ``}`);
+  const d = formatDetails(details);
+  console.log(message, d);
+  logState.unshift(`${logState.length} ${Date.now() % 1000000}: ${message} ${d}`);
 };
 
 export const logger = {
