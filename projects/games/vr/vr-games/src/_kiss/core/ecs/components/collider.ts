451af99b916b6d6731b10816817ba21f2f37b249
Rick Love <rick@toldpro.com>
2023-03-31 00:51:27 -0500

    Cleanup

--- projects/games/vr/vr-games/src/_kiss/core/ecs/components/collider.ts
+++ projects/games/vr/vr-games/src/_kiss/core/ecs/components/collider.ts
@@ -13,57 +13,56 @@ export type Entity_Collider = {
   };
 };
 
+export type Entity_Shape = Entity_ShapeBox | Entity_ShapeSphere;
+
 export const colliderComponentFactory = ({ world }: { world: World }) =>
-  createComponentFactory<Entity_ShapeBox | Entity_ShapeSphere, Entity_Collider, EntityInstance_RigidBody>()(
-    `collider`,
-    () => {
-      return {
-        addComponent: (
-          entity,
-          {
+  createComponentFactory<Entity_Shape, Entity_Collider, EntityInstance_RigidBody>()(`collider`, () => {
+    return {
+      addComponent: (
+        entity,
+        {
+          restitution,
+          friction,
+          colliderEvents,
+          sensor,
+        }: {
+          restitution?: number;
+          friction?: number;
+          colliderEvents?: boolean;
+          sensor?: boolean;
+        },
+      ) => {
+        return {
+          ...entity,
+          collider: {
             restitution,
             friction,
             colliderEvents,
             sensor,
-          }: {
-            restitution?: number;
-            friction?: number;
-            colliderEvents?: boolean;
-            sensor?: boolean;
           },
-        ) => {
-          return {
-            ...entity,
-            collider: {
-              restitution,
-              friction,
-              colliderEvents,
-              sensor,
-            },
-          };
-        },
-        setup: (entity, parent) => {
-          let colliderDesc =
-            entity.shape.kind === `box`
-              ? ColliderDesc.cuboid(...entity.shape.scale)
-              : entity.shape.kind === `sphere`
-              ? ColliderDesc.ball(entity.shape.radius)
-              : undefined;
+        };
+      },
+      setup: (entity, parent) => {
+        let colliderDesc =
+          entity.shape.kind === `box`
+            ? ColliderDesc.cuboid(...entity.shape.scale)
+            : entity.shape.kind === `sphere`
+            ? ColliderDesc.ball(entity.shape.radius)
+            : undefined;
 
-          if (!colliderDesc) throw new Error(`ColliderDesc not defined`);
+        if (!colliderDesc) throw new Error(`ColliderDesc not defined`);
 
-          if (entity.collider.restitution) {
-            colliderDesc = colliderDesc.setRestitution(entity.collider.restitution);
-          }
-          if (entity.collider.friction) {
-            colliderDesc = colliderDesc.setFriction(entity.collider.friction);
-          }
-          if (entity.collider.sensor) {
-            colliderDesc = colliderDesc.setSensor(true);
-          }
-          world.createCollider(colliderDesc, parent.rigidBody.rigidBody);
-          return entity;
-        },
-      };
-    },
-  );
+        if (entity.collider.restitution) {
+          colliderDesc = colliderDesc.setRestitution(entity.collider.restitution);
+        }
+        if (entity.collider.friction) {
+          colliderDesc = colliderDesc.setFriction(entity.collider.friction);
+        }
+        if (entity.collider.sensor) {
+          colliderDesc = colliderDesc.setSensor(true);
+        }
+        world.createCollider(colliderDesc, parent.rigidBody.rigidBody);
+        return entity;
+      },
+    };
+  });
