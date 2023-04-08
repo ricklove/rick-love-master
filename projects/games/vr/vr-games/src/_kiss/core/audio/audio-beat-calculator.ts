6948d42c4a94c78ac7dc614cd50cf102a300418d
Rick Love <rick@toldpro.com>
2023-04-08 00:23:26 -0500

    WIP trying to find beat

--- projects/games/vr/vr-games/src/_kiss/core/audio/audio-beat-calculator.ts
+++ projects/games/vr/vr-games/src/_kiss/core/audio/audio-beat-calculator.ts
@@ -6,15 +6,18 @@ export const createAudioBeatCalculator = () => {
     audio: undefined as undefined | THREE.Audio,
     audioAnalyser: undefined as undefined | THREE.AudioAnalyser,
     buckets: [] as boolean[],
+    bucketsAverages: [] as number[],
     //topBuckets: [] as number[],
     top: ``,
+    total: 0,
+    totalRunning: 0,
   };
 
   return {
     setup: (audio: THREE.Audio) => {
       state.audio = audio;
       const audioAnalyser = (state.audioAnalyser = new THREE.AudioAnalyser(audio, 1024));
-      audioAnalyser.analyser.minDecibels = -40;
+      audioAnalyser.analyser.minDecibels = -60;
       audioAnalyser.analyser.maxDecibels = -10;
       audioAnalyser.analyser.smoothingTimeConstant = 0;
     },
@@ -28,7 +31,7 @@ export const createAudioBeatCalculator = () => {
       const data = audioAnalyser.data;
 
       //   const hitIndexes = [...data]
-      //     .map((x, i) => ({ x, i }))
+      //     .map((x, i) => ({ raw: x, iRaw: i, x: Math.floor(x / 64) * 64, i: Math.floor(i / 8) * 8 }))
       //     .filter((x) => x.x > 32)
       //     .sort((a, b) => a.x - b.x)
       //     .slice(0, 2);
@@ -40,17 +43,30 @@ export const createAudioBeatCalculator = () => {
       //     state.top = topStr;
       //   }
 
-      //   const topBuckets = [...data].map
+      //   for (let i = 0; i < data.length; i++) {
+      //     const value = data[i];
 
-      for (let i = 0; i < data.length; i++) {
-        const value = data[i];
-        const isHit = value > 128;
-        const wasHit = state.buckets[i];
-        state.buckets[i] = isHit;
+      //     state.bucketsAverages[i] = (state.bucketsAverages[i] || 0) * 0.95 + value * 0.05;
+      //     const diff = Math.abs(state.bucketsAverages[i] - value);
 
-        if (isHit !== wasHit) {
-          console.log(`hit change ${isHit ? `OOO` : `...`} [${i}] @${value}`);
-        }
+      //     const isHit = diff > 64;
+      //     const wasHit = state.buckets[i];
+      //     state.buckets[i] = isHit;
+
+      //     if (isHit !== wasHit) {
+      //       console.log(`hit change ${isHit ? `OOO` : `...`} [${i}] @${value.toString(16)} ${diff.toString(16)}`);
+      //     }
+      //   }
+
+      const total = data.reduce((a, b) => a + b, 0);
+      const diffRatio = (total - state.totalRunning) / state.totalRunning;
+
+      if (diffRatio > 0.5 && total > 100) {
+        console.log(`total`, total, `diffRatio`, diffRatio, `totalRunning`, state.totalRunning);
+        state.total = total;
+        state.totalRunning = state.totalRunning * 0.95 + total * 0.05;
+      } else {
+        state.totalRunning = state.totalRunning * 0.8 + total * 0.2;
       }
 
       //   const hitIndexes = [...data].map((x, i) => ({ x, i })).filter((x) => x.x > 128);
